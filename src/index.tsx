470fd4f9c1ed20edd82bc58b74a4f2d09937d9f3
Lua <xdaddisxd@gmail.com>
2020-02-19 11:57:58 -0400

    base project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {Route, Switch} from 'react-router-dom';
+import {ConnectedRouter} from 'connected-react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+import * as serviceWorker from './serviceWorker';
+import {history} from "./store/configureStore";
+import store from './store';
+
+
+import LoginView from './Components/Login/LoginView';
+import NavbarView from './Components/Navbar/NavbarView';
+import ResetPasswordView from './Components/ResetPassword/ResetPasswordView';
+import ConfirmPasswordView from './Components/ConfirmPassword/ConfirmPasswordView';
+import SidebarView from './Components/Sidebar/SidebarView';
+import SignUpView from './Components/SignUp/SignUpView';
+import App from './App';
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+      <Route path={`/`} component={App}/>
+        <Route path={`/login`} component={LoginView}/>
+        <Route path={`/navbar`} component={NavbarView}/>
+        <Route path={`/reset-password`} component={ResetPasswordView}/>
+        <Route path={`/confirm-password`} component={ConfirmPasswordView}/>
+        <Route path={`/sidebar`} component={SidebarView}/>
+        <Route path={`/sign-up`} component={SignUpView}/>
+      </Switch>
+    </ConnectedRouter>
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/configureStore.tsx
@@ -0,0 +1,32 @@
+import {createBrowserHistory} from 'history';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {routerMiddleware} from 'connected-react-router';
+import createRootReducer from './reducers';
+import thunk from "redux-thunk";
+
+export const history = createBrowserHistory();
+
+const logger = (store: any) => (next: any) => (action: any) => {
+  // console.log('dispatching', action);
+  let result = next(action);
+  // console.log('next state', store.getState());
+  return result;
+};
+
+const composeEnhancers = compose;
+
+export default function configureStore(preloadedState: any) {
+  const store = createStore(
+    createRootReducer(history), // root reducer with router state
+    preloadedState,
+    // compose(
+    composeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history), // for dispatching history actions
+        logger,
+        thunk
+      ),
+    )
+  );
+  return store
+}
