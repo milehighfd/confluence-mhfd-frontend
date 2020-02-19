01f57de69ad4d0ced2c7e28ac57a5acf78f151b6
Ross <rosalbita15@gmail.com>
2020-02-19 17:43:50 -0400

    Add sidebar

--- src/Components/Sidebar/SidebarView.tsx
+++ src/Components/Sidebar/SidebarView.tsx
@@ -20,27 +20,33 @@ export default () => {
         <Sider collapsible collapsed={state.collapsed} onCollapse={onCollapse}>
           <Menu theme="dark" defaultSelectedKeys={['0']} mode="inline">
             <Menu.Item key="1">
-              <img src="icons/menu-white-01.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-01.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-01.svg" alt="" width="18px" />
               <span>My SMS</span>
             </Menu.Item>
             <Menu.Item key="2">
-              <img src="icons/menu-white-02.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-02.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-02.svg" alt="" width="18px" />
               <span>Map View</span>
             </Menu.Item>
             <Menu.Item key="3">
-              <img src="icons/menu-white-03.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-03.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-03.svg" alt="" width="18px" />
               <span>Create a Project</span>
             </Menu.Item>
             <Menu.Item key="4">
-              <img src="icons/menu-white-04.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-04.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-04.svg" alt="" width="18px" />
               <span>Work Request</span>
             </Menu.Item>
             <Menu.Item key="5">
-              <img src="icons/menu-white-05.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-05.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-05.svg" alt="" width="18px" />
               <span>Work Plan</span>
             </Menu.Item>
             <Menu.Item key="6">
-              <img src="icons/menu-white-06.svg" alt=""/>
+              <img className="img-h" src="icons/menu-white-06.svg" alt="" width="18px" />
+              <img className="img-a" src="icons/menu-green-06.svg" alt="" width="18px" />
               <span>Settings</span>
             </Menu.Item>
           </Menu>
