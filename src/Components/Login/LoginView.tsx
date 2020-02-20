ca721e8820603979e8c98101f7449f23c1b83f2e
martin guachalla <martinguachalla@gmail.com>
2020-02-20 15:57:59 -0400

    add style in login

--- src/Components/Login/LoginView.tsx
+++ src/Components/Login/LoginView.tsx
@@ -22,18 +22,31 @@ export default () => {
       </Carousel>        
       </Col>
       <Col span={11}>
-   
       <div className="login-step01">
+      <Row className="returnText">
+          <Col span={12}>
+          <Button shape="circle" icon="arrow-left" /><span>Back</span>
+          </Col>
+          <Col span={12} style={{ textAlign: 'right' }}>
+          <span>Continue as Guest</span><Button shape="circle" icon="arrow-right" />
+          </Col>
+      </Row>
       <Form style={{ width: '533px' }}  className="login-form">
       <h1>
         Welcome to MHFD's Confluence
       </h1>
-      <Form.Item>
-        <Input placeholder="Email Adress"/>
-      </Form.Item>
-      <Form.Item>
-        <Input placeholder="Password"/>
-      </Form.Item>
+      <div className="group">      
+        <input type="text" required/>
+        <span className="highlight"></span>
+        <span className="bar"></span>
+        <label>Email Address</label>
+      </div>
+      <div className="group">      
+        <input type="text" required/>
+        <span className="highlight"></span>
+        <span className="bar"></span>
+        <label>Enter Password</label>
+      </div>
       <Form.Item>
         <span>Don’t have an account?</span>
           <a className="login-form-forgot" href="">
