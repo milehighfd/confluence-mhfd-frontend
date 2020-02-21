2023ba3b8ad34490c7bac0ed281a15fb07e7f9b3
Lua <xdaddisxd@gmail.com>
2020-02-21 08:42:37 -0400

    map update design

--- src/Components/Map/MapView.tsx
+++ src/Components/Map/MapView.tsx
@@ -1,12 +1,21 @@
 import * as React from "react";
+import { Layout } from 'antd';
+
+
 import NavbarView from "../Navbar/NavbarView";
 import SidebarView from "../Sidebar/SidebarView";
 
 
 export default () => {
   return <>
+        <Layout>
           <NavbarView></NavbarView>
-          <SidebarView></SidebarView>
-          <div> Map up</div>
+          <Layout>
+            <SidebarView></SidebarView>
+            <Layout>
+              <div> I tried Ross, I tried</div>
+            </Layout>
+          </Layout>
+        </Layout>
         </>
 }
