2f9ca89682aa4feb411f241b16e17d6cdc99e9b5
Ross <rosalbita15@gmail.com>
2020-02-27 17:30:35 -0400

    Add pagination

--- src/Components/User/UserView.tsx
+++ src/Components/User/UserView.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Layout, Row, Col, Tabs, Input, Menu, Dropdown, Button, Icon, Collapse, Radio, Switch} from 'antd';
+import {Layout, Row, Col, Tabs, Input, Menu, Dropdown, Button, Icon, Collapse, Radio, Switch, Table, Divider, Tag, Pagination} from 'antd';
 import NavbarView from "../Navbar/NavbarContainer";
 import SidebarView from "../Sidebar/SidebarContainer";
 
@@ -36,7 +36,6 @@ const genExtra = () => (
     </Col>
     <Col span={1} style={{textAlign: 'right'}}><img src="Icons/icon-20.svg" alt=""/></Col>
   </Row>
-
 );
 
 export default () => {
@@ -207,6 +206,7 @@ export default () => {
                               </div>
                             </Panel>
                           </Collapse>
+                          <Pagination defaultCurrent={1} total={200} />
                         </TabPane>
 
                         <TabPane tab="Pending User Requests" key="2">
@@ -285,43 +285,43 @@ export default () => {
                                 <Col className="gutter-row" span={4}>
                                   <div className="user-card">
                                     <p><Radio></Radio></p>
+                                    <div className="user-d"><h6>MHFD Admin</h6></div>
+                                  </div>
+                                </Col>
+                                <Col className="gutter-row" span={4}>
+                                  <div className="user-card">
+                                    <p><Radio></Radio></p>
                                     <div className="user-d"><h6>MHFD Staff</h6></div>
                                   </div>
                                 </Col>
                                 <Col className="gutter-row" span={4}>
                                   <div className="user-card">
-                                    <div className="user-d"><h6> Consultant/ Contractor</h6></div>
                                     <p><Radio></Radio></p>
-                                  </div>
-                                </Col>
-                                <Col className="gutter-row" span={4}>
-                                  <div className="user-card">
-                                    <div className="user-d">
-                                      <h6>Local</h6>
-                                      <h6>Government</h6>
-                                    </div>
-                                    <p><Radio></Radio></p>
-                                  </div>
-                                </Col>
-                                <Col className="gutter-row" span={4}>
-                                  <div className="user-card">
-                                    <div className="user-d"><h6>Other</h6></div>
-                                    <p><Radio></Radio></p>
-                                  </div>
-                                </Col>
-                                <Col className="gutter-row" span={4}>
-                                  <div className="user-card">
                                     <div className="user-d">
                                       <h6>Local </h6>
                                       <h6>Government Admin</h6>
                                     </div>
-                                    <p><Radio></Radio></p>
                                   </div>
                                 </Col>
                                 <Col className="gutter-row" span={4}>
                                   <div className="user-card">
-                                    <div className="user-d"><h6>MHFD Admin</h6></div>
                                     <p><Radio></Radio></p>
+                                    <div className="user-d">
+                                      <h6>Local</h6>
+                                      <h6>Government</h6>
+                                    </div>
+                                  </div>
+                                </Col>
+                                <Col className="gutter-row" span={4}>
+                                  <div className="user-card">
+                                    <p><Radio></Radio></p>
+                                    <div className="user-d"><h6> Consultant/ Contractor</h6></div>
+                                  </div>
+                                </Col>
+                                <Col className="gutter-row" span={4}>
+                                  <div className="user-card">
+                                    <p><Radio></Radio></p>
+                                    <div className="user-d"><h6>Other</h6></div>
                                   </div>
                                 </Col>
                               </Row>
@@ -365,8 +365,47 @@ export default () => {
                             </div>
                           </Panel>
                         </Collapse>
+
+                        <Pagination defaultCurrent={1} total={200} />
                         </TabPane>
+
                         <TabPane tab="User Activity" key="3">
+                          <Row className="activity-h">
+                            <Col span={5}><Button>Data and Time <img src="Icons/icon-14.svg" alt=""/></Button></Col>
+                            <Col span={5}><Button>User <img src="Icons/icon-14.svg" alt=""/></Button></Col>
+                            <Col span={5}><Button>City <img src="Icons/icon-14.svg" alt=""/></Button></Col>
+                            <Col span={5}><Button>Change <img src="Icons/icon-14.svg" alt=""/></Button></Col>
+                          </Row>
+
+                          <Row className="activity-b">
+                            <Col span={5}>09/16/2019, 06:17PM</Col>
+                            <Col span={5}>Jovanna Maiani</Col>
+                            <Col span={5}>Westminster</Col>
+                            <Col span={5}><a href="">User Login</a></Col>
+                          </Row>
+
+                          <Row className="activity-b">
+                            <Col span={5}>09/16/2019, 06:17PM</Col>
+                            <Col span={5}>Jovanna Maiani</Col>
+                            <Col span={5}>Westminster</Col>
+                            <Col span={5}><a href="">User Login</a></Col>
+                          </Row>
+
+                          <Row className="activity-b">
+                            <Col span={5}>09/16/2019, 06:17PM</Col>
+                            <Col span={5}>Jovanna Maiani</Col>
+                            <Col span={5}>Westminster</Col>
+                            <Col span={5}><a href="">User Login</a></Col>
+                          </Row>
+
+                          <Row className="activity-b">
+                            <Col span={5}>09/16/2019, 06:17PM</Col>
+                            <Col span={5}>Jovanna Maiani</Col>
+                            <Col span={5}>Westminster</Col>
+                            <Col span={5}><a href="">User Login</a></Col>
+                          </Row>
+
+                          <Pagination defaultCurrent={1} total={200} />
                         </TabPane>
                       </Tabs>
                     </Col>
