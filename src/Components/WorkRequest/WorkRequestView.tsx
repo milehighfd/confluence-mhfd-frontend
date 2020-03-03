fe1562a8cf4c55a24d900e9c9ccd7e6076f3267b
Ross <rosalbita15@gmail.com>
2020-03-03 16:34:09 -0400

    Add work request

--- src/Components/WorkRequest/WorkRequestView.tsx
+++ src/Components/WorkRequest/WorkRequestView.tsx
@@ -49,15 +49,87 @@ const menu = (
 export default () => {
   const [visible, setVisible] = useState(false);
   const chat =  <Drawer
-    title="Basic Drawer"
     placement="right"
     closable={false}
     onClose={() => setVisible(false)}
     visible={visible}
+    className="chat-r"
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-    <p>Some contents...</p>
+      <h5>Team Collaborators</h5>
+      <Row>
+        <Col span={4}>
+          <img src="/Icons/icon-28.svg" alt="" height="41px"/>
+        </Col>
+        <Col span={13}>
+          <h6>Jon Villines</h6>
+          <p>Project Manager</p>
+        </Col>
+        <Col span={7} style={{textAlign: 'right'}}>
+          <span>MHFD</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={4}>
+          <img src="/Icons/icon-28.svg" alt="" height="41px"/>
+        </Col>
+        <Col span={13}>
+          <h6>Carolyn Roan</h6>
+          <p>Floodplain Administration</p>
+        </Col>
+        <Col span={7} style={{textAlign: 'right'}}>
+          <span>City of Littleton</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={4}>
+          <img src="/Icons/icon-28.svg" alt="" height="41px"/>
+        </Col>
+        <Col span={13}>
+          <h6>Deb Ohlinger</h6>
+          <p>People Manager</p>
+        </Col>
+        <Col span={7} style={{textAlign: 'right'}}>
+          <span>Olsson</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={4}>
+          <img src="/Icons/icon-28.svg" alt="" height="41px"/>
+        </Col>
+        <Col span={13}>
+          <h6>Amy Gabor</h6>
+          <p>Project Engineer</p>
+        </Col>
+        <Col span={7} style={{textAlign: 'right'}}>
+          <span>Olsson</span>
+        </Col>
+      </Row>
+      <div className="chat-00">
+        <div className="chat-head">
+          Comments <img src="/Icons/icon-19.svg" alt="" height="20px"/>
+        </div>
+        <div className="chat-body">
+          <img src="/Icons/icon-61.svg" alt=""/>
+          <h6>Share your thoughts</h6>
+          <p>
+            Let everyone in your group know
+            what you think about this listing
+          </p>
+        </div>
+        <div className="chat-footer">
+        <Row>
+          <Col span={4}>
+            <img src="/Icons/icon-28.svg" alt="" height="41px"/>
+          </Col>
+          <Col span={13}>
+            <Input placeholder="Add a coment..." />
+          </Col>
+          <Col span={7} style={{textAlign: 'right'}}>
+            <Button className="btn-send">SEND</Button>
+          </Col>
+        </Row>
+        </div>
+      </div>
   </Drawer>
   return <>
         {chat}
