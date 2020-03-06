122bbd69cb560107353dabbb0425cb547e12d48d
Ross <rosalbita15@gmail.com>
2020-03-06 12:24:24 -0400

    edit

--- src/Components/Profile/ProfileView.tsx
+++ src/Components/Profile/ProfileView.tsx
@@ -1,7 +1,49 @@
-import React, {useState} from "react";
-import { Layout} from 'antd';
+import * as React from "react";
+import { Layout, Row, Col, Collapse, Dropdown, Icon, Menu, Button, Tabs, Tag, Card, Input, Progress, Timeline,Checkbox, Select, Radio} from 'antd';
 
+
+import NavbarView from "../Navbar/NavbarView";
+import SidebarView from "../Sidebar/SidebarView";
+import MapFilterView from '../MapFilter/MapFilterView';
+import MapTypesView from "../MapTypes/MapTypesView";
+
+const { Panel } = Collapse;
+const ButtonGroup = Button.Group;
+const { Meta } = Card;
+const { TabPane } = Tabs;
+const { Search } = Input;
+const { Option } = Select;
+
+const menu = (
+  <Menu>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="">
+        1st menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="">
+        2nd menu item
+      </a>
+    </Menu.Item>
+    <Menu.Item>
+      <a target="_blank" rel="noopener noreferrer" href="">
+        3rd menu item
+      </a>
+    </Menu.Item>
+  </Menu>
+);
 
 export default () => {
- return <Layout>se que lees mis pensamientos</Layout>
-};
+  return <>
+        <Layout>
+          <NavbarView></NavbarView>
+          <Layout>
+            <SidebarView></SidebarView>
+            <Layout className="map-00" style={{height: 'calc(100vh - 58px)'}}>
+              
+            </Layout>
+          </Layout>
+        </Layout>
+        </>
+}
