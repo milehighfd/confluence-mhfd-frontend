d8e4a2cfdba9cb269d6f7fa6ae30349a51c3a684
martin guachalla <martinguachalla@gmail.com>
2020-03-11 11:03:44 -0400

    add styles in profile

--- src/Components/Profile/ProfileView.tsx
+++ src/Components/Profile/ProfileView.tsx
@@ -36,6 +36,10 @@ const menu = (
   </Menu>
 );
 
+const genExtra = () => (
+  <img src="Icons/icon-20.svg" alt=""/>
+);
+
 const cardInformationProblems: Array<any> = [
   {
     image: "/Icons/eje.png", field1: "West Tollagate Creek GSB Drops", field2: "Westminster", field3: "$400,500",
@@ -151,9 +155,9 @@ export default () => {
           <NavbarView></NavbarView>
           <Layout>
             <SidebarView></SidebarView>
-            <Layout className="map-00" style={{height: 'calc(100vh - 58px)'}}>
+            <Layout className="map-00" style={{height: 'calc(100vh - 58px)', overflowY: 'scroll'}}>
               <Row className="profile-header">
-                <Col span={15} className="profile-info">
+                <Col span={13} className="profile-info">
                   <img className="profile-img" src="/Icons/icon-28.svg" alt=""/>
                   <div className="profile-dat">
                     <div className="profile-name">
@@ -180,7 +184,7 @@ export default () => {
                  
                 </div>
                 </Col>
-                <Col span={9}>
+                <Col span={11}>
                     <div className="profile-table">
                       <Table size={"small"} columns={columns01} dataSource={data01} pagination={false} />
                     </div>
@@ -188,23 +192,118 @@ export default () => {
                 <div className="profile-divider"></div>
               </Row>
               <Row>
-                <Col className="profile-tabs" span={15}>
+                <Col className="profile-tabs" span={17}>
                   <Tabs style={{padding:'0 53px'}} defaultActiveKey="1" className="tabs-map">
                       <TabPane tab="Problems" key="1">
-                      <Row style={{background: '#fff'}} className="card-map" gutter={[16, 16]}>
+                      <Row style={{background: '#fff', marginTop: '20px'}} className="card-map" gutter={[16, 16]}>
+                      <div className="user-filter">
+                      <div>
+                          <Search
+                          placeholder="Search by Name"
+                          onSearch={value => console.log(value)}
+                          style={{ width: 240 }}
+                          />
+                      </div>
+                      <div className="btn-r">
+                          <label>Sort by:</label>
+                          <Dropdown overlay={menu}>
+                          <Button>
+                              Approval Date <img src="Icons/icon-14.svg" alt=""/>
+                          </Button>
+                          </Dropdown>
+                      </div>
+                      </div>
                         {cardInformationProblems.map((data: any) => {
                               return <CardInformationView data={data} type={"Problems"} />
                         })}
                       </Row>
                       </TabPane>
                       <TabPane tab="Projects" key="2">
-                        {cardInformationProjects.map((data: any) => {
-                          return <CardInformationView data={data} type={"Projects"} />
-                        })}
+                        <Row style={{background: '#fff', marginTop: '20px'}} gutter={[16, 16]}>
+                          <div className="user-filter">
+                            <div>
+                                <Search
+                                placeholder="Search by Name"
+                                onSearch={value => console.log(value)}
+                                style={{ width: 240 }}
+                                />
+                            </div>
+                            <div className="btn-r">
+                                <label>Sort by:</label>
+                                <Dropdown overlay={menu}>
+                                <Button>
+                                    Approval Date <img src="Icons/icon-14.svg" alt=""/>
+                                </Button>
+                                </Dropdown>
+                            </div>
+                          </div>
+                          {cardInformationProjects.map((data: any) => {
+                            return <CardInformationView data={data} type={"Projects"} />
+                          })}
+                        </Row>
                       </TabPane>
                   </Tabs>
                 </Col>
-                <Col span={9}></Col>
+                <Col style={{paddingLeft: '15px', height: '100%'}} span={7}>
+                    <Row style={{background: '#fff', height: '100%'}}>
+                      <Col className="profile-pad">
+                        <h2>Project Collaborators</h2>
+                        <Collapse accordion
+                          bordered={false}
+                          defaultActiveKey={['1']}
+                          className="site-collapse-custom-collapse">
+                          <Panel header="West Tollgate Creek GSB Drops" key="1" className="site-collapse-custom-panel" extra={genExtra()}>
+                            <div className="chat-r pad-collab">
+                              <Row>
+                                <Col span={3}>
+                                  <img src="/Icons/icon-28.svg" alt="" height="35px"/>
+                                </Col>
+                                <Col span={14}>
+                                  <h6>Jon Villines</h6>
+                                  <p>Project Manager</p>
+                                </Col>
+                                <Col span={7} style={{textAlign: 'right'}}>
+                                  <span>MHFD</span>
+                                </Col>
+                              </Row>
+                            </div>
+                          </Panel>
+                          <Panel header="Piney Creek Channel Restoration" key="2" className="site-collapse-custom-panel" extra={genExtra()}>
+                            <div className="chat-r pad-collab">
+                              <Row>
+                                <Col span={3}>
+                                  <img src="/Icons/icon-28.svg" alt="" height="35px"/>
+                                </Col>
+                                <Col span={14}>
+                                  <h6>Jon Villines</h6>
+                                  <p>Project Manager</p>
+                                </Col>
+                                <Col span={7} style={{textAlign: 'right'}}>
+                                  <span>MHFD</span>
+                                </Col>
+                              </Row>
+                            </div>
+                          </Panel>
+                          <Panel header="Murphy Creek Bank Stabilization" key="3" className="site-collapse-custom-panel" extra={genExtra()}>
+                            <div className="chat-r pad-collab">
+                              <Row>
+                                <Col span={3}>
+                                  <img src="/Icons/icon-28.svg" alt="" height="35px"/>
+                                </Col>
+                                <Col span={14}>
+                                  <h6>Jon Villines</h6>
+                                  <p>Project Manager</p>
+                                </Col>
+                                <Col span={7} style={{textAlign: 'right'}}>
+                                  <span>MHFD</span>
+                                </Col>
+                              </Row>
+                            </div>
+                          </Panel>
+                        </Collapse>
+                      </Col>
+                    </Row> 
+                </Col>
               </Row>
             </Layout>
           </Layout>
