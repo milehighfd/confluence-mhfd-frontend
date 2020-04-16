cdc82e2835c4ce640bdc3ff2785034747411a22e
Juan Ignacio Reyes <juanireyes182@gmail.com>
2020-04-16 15:47:51 -0400

    Drag and Drop Principal Functionality

--- src/Components/Shared/DraftPanel/DraftPanel.tsx
+++ src/Components/Shared/DraftPanel/DraftPanel.tsx
@@ -1,5 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dropdown, Button, Menu } from 'antd';
+
+import { ReactSortable } from 'react-sortablejs';
+
+const SORTABLE_ITEMS = [
+  {
+    id: 'kyei281238',
+    requestName: 'West Tollgate Creek GSB Drops',
+    totalCost: '$410,000',
+    county: 'Aurora',
+    status: 'Draft'
+  },
+  {
+    id: 'asd123c3t1',
+    requestName: 'Westminster Creek GSB Drops',
+    totalCost: '$410,000',
+    county: 'Aurora',
+    status: 'Draft'
+  },
+  {
+    id: 'xx2131ff12',
+    requestName: 'Denver River Fix',
+    totalCost: '$410,000',
+    county: 'Aurora',
+    status: 'Draft'
+  },
+  {
+    id: 'x12321e213',
+    requestName: 'Potato Town',
+    totalCost: '$410,000',
+    county: 'Aurora',
+    status: 'Draft'
+  }
+];
+
+const SELECTED_ITEMS = [
+  {
+    id: 'xasd2fg234',
+    requestName: 'Mac n Cheese',
+    totalCost: '$120,000',
+    county: 'Denver',
+    status: 'Draft'
+  }
+]
 
 const menu = (
   <Menu className="menu-card">
@@ -22,46 +65,138 @@ const menu = (
 );
 
 export default ({ headers } : { headers : Array<string | number> }) => {
+  const [state, setState] = useState(SORTABLE_ITEMS);
+  const [isDragging, setIsDragging] = useState(false);
+  const [selectedState, setSelectedState] = useState(SELECTED_ITEMS);
+  const [containerClass, setContainerClass] = useState('col-wr');
+  const [dragged, setDragged] = useState({ id: '', parent: '' });
+
+  const handleDragEnter = (trigger : string | number) => {
+    if (isDragging) {
+      setDragged({ ...dragged, parent: '' + trigger })
+    }
+  }
+
+  const handleDragLeave = (event : any) => {
+    if (isDragging) {
+    }
+  }
+
+  const handleStartDrag = (event : any, trigger : string | number) => {
+    const index : number = event.oldIndex;
+    if (trigger === 'WORKSPACE') {
+      setDragged({id: state[index].id, parent: '' + trigger}); 
+    } else {
+      setDragged({id: selectedState[index].id, parent: '' + trigger});
+    }
+
+    setContainerClass('col-wr col-hovered');
+    setIsDragging(true);
+  }
+
+  const handleEndDrag = (event : any, trigger : string | number) => {
+    if (!event.pullMode && '' + trigger !== dragged.parent) {
+      if (trigger === 'WORKSPACE') {
+        const item : any = state.find((x : any) => x.id === dragged.id);
+        const newState = [...selectedState];
+        newState.push(item);
+        setSelectedState(newState);
+        state.splice(event.oldIndex, 1);
+      } else {
+        const item : any = selectedState.find((x : any) => x.id === dragged.id);
+        const newState = [...state];
+        newState.push(item);
+        setState(newState);
+        selectedState.splice(event.oldIndex, 1);
+      }
+    }
+
+    setContainerClass('col-wr');
+    setIsDragging(false);
+  }
+
+  const getContainerStyle = (header : string | number) => {
+    if ('' + header === dragged.parent && isDragging) {
+      return {
+        borderColor: 'green',
+        backgroundColor: '#12a80d25'
+      };
+    } else {
+      return {};
+    }
+  }
+
   return (
     <div className="work-request">
       <div>
         <h3>Workspace</h3>
-        <div className="col-wr">
+        <div 
+          className={containerClass}
+          style={getContainerStyle('WORKSPACE')}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={() => handleDragEnter('WORKSPACE')}
+          onDragLeave={() => handleDragLeave('WORKSPACE')} >
           <Button className="btn-create"><img src="/Icons/icon-18.svg" alt="" /> Create Project</Button>
-          <div className="card-wr">
-            <h4>West Tollgate Creek GSB Drops </h4>
-            <h6>$410,000</h6>
-            <p>Aurora <label>Draft</label></p>
-            <Dropdown overlay={menu} className="menu-wr">
-              <span className="ant-dropdown-link" style={{ cursor: 'pointer' }}>
-                <img src="/Icons/icon-60.svg" alt="" />
-              </span>
-            </Dropdown>
-          </div>
-        </div>
-      </div>
-
-      {headers.map((header : string | number) => (
-        <div>
-          <h3>{header}</h3>
-          <div className="col-wr">
-            { header === headers[0] ?
-              <div className="card-wr">
-                <h4>West Tollgate Creek GSB Drops </h4>
-                <h6>$410,000</h6>
-                <p>Aurora <label>Draft</label></p>
+          <ReactSortable 
+            list={state} 
+            setList={setState} 
+            animation={200}
+            onStart={(e) => handleStartDrag(e, 'WORKSPACE')}
+            onEnd={(e) => handleEndDrag(e, 'WORKSPACE')}
+            group="capital" >
+            {state.map((item : any) => (
+              <div className="card-wr" key={item.id}>
+                <h4>{item.requestName} </h4>
+                <h6>{item.totalCost}</h6>
+                <p>{item.county} <label>{item.status}</label></p>
                 <Dropdown overlay={menu} className="menu-wr">
                   <span className="ant-dropdown-link" style={{ cursor: 'pointer' }}>
                     <img src="/Icons/icon-60.svg" alt="" />
                   </span>
                 </Dropdown>
               </div>
-                :
+            ))}
+          </ReactSortable>
+        </div>
+      </div>
+      
+      {headers.map((header: string | number, index: number) => (
+        <div key={index}>
+          <h3>{header}</h3>
+          <div 
+            className={"col-wr " + containerClass}
+            style={getContainerStyle(header)}
+            onDragOver={(e) => e.preventDefault()}
+            onDragEnter={() => handleDragEnter(header)}
+            onDragLeave={() => handleDragLeave(header)} >
+            {header === headers[0] ?
+              <ReactSortable
+                list={selectedState}
+                setList={setSelectedState}
+                animation={200}
+                onStart={(e) => handleStartDrag(e, header)}
+                onEnd={(e) => handleEndDrag(e, header)}
+                group="capital" >
+                {selectedState.map((item : any) => (
+                  <div className="card-wr" key={item.id}>
+                    <h4>{item.requestName} </h4>
+                    <h6>{item.totalCost}</h6>
+                    <p>{item.county} <label>{item.status}</label></p>
+                    <Dropdown overlay={menu} className="menu-wr">
+                      <span className="ant-dropdown-link" style={{ cursor: 'pointer' }}>
+                        <img src="/Icons/icon-60.svg" alt="" />
+                      </span>
+                    </Dropdown>
+                  </div>
+                ))}
+              </ReactSortable>
+              :
               null
             }
           </div>
         </div>
       ))}
+
     </div>
   )
 }
--- src/Components/WorkRequest/WorkRequestView.tsx
+++ src/Components/WorkRequest/WorkRequestView.tsx
@@ -1,261 +1,73 @@
 import React, { useState } from "react";
-import { Row, Col, Button, Tabs, Input, Drawer } from 'antd';
+import { Row, Col, Button, Tabs, Input } from 'antd';
 
 import mapFormContainer from "../../hoc/mapFormContainer";
 import DraftPanel from '../Shared/DraftPanel/DraftPanel';
 
 import { DEFAULT_HEADERS, MAINTENANCE_HEADERS, SPECIAL_HEADERS } from '../../constants/constants'
+import Chat from "../Shared/Chat/Chat";
 
 const { TabPane } = Tabs;
 
 export const WorkRequest = () => {
   const [visible, setVisible] = useState(false);
-
-  const chat = (
-    <Drawer
-      placement="right"
-      closable={false}
-      onClose={() => setVisible(false)}
-      visible={visible}
-      className="chat-r"
-    >
-      <h5>Team Collaborators</h5>
-      <Row>
-        <Col span={4}>
-          <img src="/Icons/icon-28.svg" alt="" height="35px" />
-        </Col>
-        <Col span={13}>
-          <h6>Jon Villines</h6>
-          <p>Project Manager</p>
-        </Col>
-        <Col span={7} style={{ textAlign: 'right' }}>
-          <span>MHFD</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={4}>
-          <img src="/Icons/icon-28.svg" alt="" height="35px" />
-        </Col>
-        <Col span={13}>
-          <h6>Carolyn Roan</h6>
-          <p>Floodplain Administration</p>
-        </Col>
-        <Col span={7} style={{ textAlign: 'right' }}>
-          <span>City of Littleton</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={4}>
-          <img src="/Icons/icon-28.svg" alt="" height="35px" />
-        </Col>
-        <Col span={13}>
-          <h6>Deb Ohlinger</h6>
-          <p>People Manager</p>
-        </Col>
-        <Col span={7} style={{ textAlign: 'right' }}>
-          <span>Olsson</span>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={4}>
-          <img src="/Icons/icon-28.svg" alt="" height="35px" />
-        </Col>
-        <Col span={13}>
-          <h6>Amy Gabor</h6>
-          <p>Project Engineer</p>
-        </Col>
-        <Col span={7} style={{ textAlign: 'right' }}>
-          <span>Olsson</span>
-        </Col>
-      </Row>
-      <div className="chat-00">
-        <div className="chat-head">
-          Comments <img src="/Icons/icon-19.svg" alt="" height="20px" />
-        </div>
-        <div className="chat-body">
-          <img src="/Icons/icon-61.svg" alt="" />
-          <h6>Share your thoughts</h6>
-          <p>
-            Let everyone in your group know
-            what you think about this listing
-          </p>
-        </div>
-        <div className="chat-footer">
-          <Row>
-            <Col span={4}>
-              <img src="/Icons/icon-28.svg" alt="" height="35px" />
-            </Col>
-            <Col span={13}>
-              <Input placeholder="Add a comment..." />
-            </Col>
-            <Col span={7} style={{ textAlign: 'right' }}>
-              <Button className="btn-send">SEND</Button>
-            </Col>
-          </Row>
-        </div>
-      </div>
-    </Drawer>
-  ); 
+  const [tabPosition, setTabPosition] = useState("1");
 
   return <>
-    {chat}
+    <Chat visible={visible} setVisible={setVisible} />
     <div className="count request">
       <Row>
         <Col span={14}><h2>Boulder County Work Request</h2></Col>
         <Col span={10} style={{ textAlign: 'right' }}>
           <Button className="btn-request"><img src="/Icons/icon-01.svg" alt="" /></Button>
           <Button className="btn-request"><img src="/Icons/icon-02.svg" alt="" /></Button>
-          <Button className="btn-comment" onClick={() => { setVisible(true); console.log('click ', visible); }}><img src="/Icons/icon-03.svg" alt="" /> Comments</Button>
+          <Button className="btn-comment" onClick={() => setVisible(true)}><img src="/Icons/icon-03.svg" alt="" /> Comments</Button>
         </Col>
       </Row>
 
-      <Tabs defaultActiveKey="1" className="tabs-map">
+      <Tabs activeKey={tabPosition} onChange={(key) => setTabPosition(key)} className="tabs-map">
         <TabPane tab="Capital" key="1">
           <DraftPanel headers={DEFAULT_HEADERS} />
-          <div className="cost-wr">
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Target Cost</h5></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Differential</h5></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-            </Row>
-            <Row style={{ padding: '0px 20px' }}>
-              <Col span={24} style={{ textAlign: 'right' }}>
-                <Button>Submit to Admin</Button>
-              </Col>
-            </Row>
-          </div>
-
         </TabPane>
         <TabPane tab="Study" key="2">
           <DraftPanel headers={DEFAULT_HEADERS} />
-
-          <div className="cost-wr">
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Target Cost</h5></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Differential</h5></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-            </Row>
-            <Row style={{ padding: '0px 20px' }}>
-              <Col span={24} style={{ textAlign: 'right' }}>
-                <Button>Submit to Admin</Button>
-              </Col>
-            </Row>
-          </div>
         </TabPane>
         <TabPane tab="Maintenance" key="3">
           <DraftPanel headers={MAINTENANCE_HEADERS} />
-
-          <div className="cost-wr">
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Target Cost</h5></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Differential</h5></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-            </Row>
-            <Row style={{ padding: '0px 20px' }}>
-              <Col span={24} style={{ textAlign: 'right' }}>
-                <Button>Submit to Admin</Button>
-              </Col>
-            </Row>
-          </div>
         </TabPane>
         <TabPane tab="Acquisition" key="4">
           <DraftPanel headers={SPECIAL_HEADERS} />
-
-          <div className="cost-wr">
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Target Cost</h5></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Differential</h5></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-            </Row>
-            <Row style={{ padding: '0px 20px' }}>
-              <Col span={24} style={{ textAlign: 'right' }}>
-                <Button>Submit to Admin</Button>
-              </Col>
-            </Row>
-          </div>
         </TabPane>
         <TabPane tab="Special" key="5">
           <DraftPanel headers={SPECIAL_HEADERS} />
-
-          <div className="cost-wr">
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-              <Col span={6}><Input placeholder="Total cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Target Cost</h5></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-              <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
-            </Row>
-            <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
-              <Col span={6}><h5>Differential</h5></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-              <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
-            </Row>
-            <Row style={{ padding: '0px 20px' }}>
-              <Col span={24} style={{ textAlign: 'right' }}>
-                <Button>Submit to Admin</Button>
-              </Col>
-            </Row>
-          </div>
         </TabPane>
       </Tabs>
+
+      <div className="cost-wr">
+        <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
+          <Col span={6}><h5>TOTAL REQUESTED</h5></Col>
+          <Col span={6}><Input placeholder="Total cost" /></Col>
+          <Col span={6}><Input placeholder="Total cost" /></Col>
+          <Col span={6}><Input placeholder="Total cost" /></Col>
+        </Row>
+        <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
+          <Col span={6}><h5>Target Cost</h5></Col>
+          <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
+          <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
+          <Col span={6}><Input className="input-pp" placeholder="Enter target cost" /></Col>
+        </Row>
+        <Row gutter={[16, 24]} style={{ padding: '0px 20px' }}>
+          <Col span={6}><h5>Differential</h5></Col>
+          <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
+          <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
+          <Col span={6}><Input className="input-rr" placeholder="XXX Difference" /></Col>
+        </Row>
+        <Row style={{ padding: '0px 20px' }}>
+          <Col span={24} style={{ textAlign: 'right' }}>
+            <Button>Submit to Admin</Button>
+          </Col>
+        </Row>
+      </div>
 
     </div>
   </>
