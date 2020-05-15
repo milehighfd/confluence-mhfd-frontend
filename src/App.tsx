e6dde43e4e0dc85ae0d986a509d658ef0fd079da
herty <herbertwilmer@gmail.com>
2020-05-15 16:52:04 -0400

    adding loader

--- src/App.tsx
+++ src/App.tsx
@@ -92,6 +92,4 @@ function App({ replaceAppUser } : { replaceAppUser : Function }) {
 
 }
 
-
-
 export default App;
--- src/Components/Loading/LoadingView.tsx
+++ src/Components/Loading/LoadingView.tsx
@@ -3,7 +3,20 @@ import 'antd/dist/antd.css';
 import { Spin } from 'antd';
 
 export default () => {
-  return <div>
-      <Spin size="large" />  
+  // return <div>
+  //     <Spin size="large" />  
+  // </div>
+  return <div className="loading">
+    <div className="loader">
+      <svg>
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="2" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 5 -2" result="gooey" />
+            <feComposite in="SourceGraphic" in2="gooey" operator="atop"/>
+          </filter>
+        </defs>
+      </svg>
+    </div>
   </div>
 }
