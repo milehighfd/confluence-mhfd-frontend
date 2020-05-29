0afcade905e8e5338045d02d8fa19f87b651d053
Herbert Wilmer Quispe Churqui <herbertwilmer@gmail.com>
2020-05-29 13:45:18 -0400

    add style width to icon image

--- src/Components/UploadAttachment/UploadAttachmentView.tsx
+++ src/Components/UploadAttachment/UploadAttachmentView.tsx
@@ -21,7 +21,7 @@ export default ({ attachments, uploadFile, getAllAttachment, removeAttachment }:
       dataIndex: 'filename',
       render: (filename: { filename: string, mimetype: string, value: string}) => {
         return (<div style={{ display: 'flex' }}>
-          <div style={{ padding: '6px 0px' }}> {filename.mimetype.includes('image/') ? <img alt="example" className="img-up" src={filename.value} height="30px" />:
+          <div style={{ padding: '6px 0px' }}> {filename.mimetype.includes('image/') ? <img  className="img-up" src={filename.value} height="30px" width="30px" />:
            filename.mimetype === 'application/pdf' ? <FilePdfTwoTone /> : <FileTwoTone />}  {filename.filename} </div></div>)
       },
       sorter: true
