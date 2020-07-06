42b7a8565ce3e657d74d40ac5f0aa3521779ce55
Bladimir Calcina <bcalcinac@outlook.com>
2020-07-06 13:58:50 -0400

    change detail page for projects

--- src/Components/Shared/Modals/DetailedInfo.tsx
+++ src/Components/Shared/Modals/DetailedInfo.tsx
@@ -21,17 +21,17 @@ export default ({ detailedPage }: { detailedPage: Detailed }) => {
         </Col>
       </Row>
       <Row>
-      <Col span={4}>
+        <Col span={4}>
           <label><i>Source Name</i></label>
         </Col>
         <Col span={8}>
-          <p>{detailed.source}</p>
+          <p>{detailed.sourcename ? detailed.sourcename : ''}</p>
         </Col>
         <Col span={4}>
           <label><i>Source</i></label>
         </Col>
         <Col span={8}>
-          <p>{detailed.sourcename}</p>
+          <p>{detailed.source ? detailed.source : ''}</p>
         </Col>
       </Row>
       <Row>
@@ -51,70 +51,105 @@ export default ({ detailedPage }: { detailedPage: Detailed }) => {
         <Col span={8}>
           <p>{detailed.streamname}</p>
         </Col>
-        { detailed.projectsubtype !== 'Debris Management' ? <>
+        { detailed.projecttype === 'Capital' ? <>
         <Col span={4}>
           <label><i>Start Year</i></label>
         </Col>
         <Col span={8}>
-          <p>{detailed.startyear ? detailed.startyear : ''}</p>
+          <p>{detailed.startyear ? detailed.startyear : '-'}</p>
         </Col>
         </> : <>
         <Col span={4}>
           <label><i>Subtype</i></label>
         </Col>
         <Col span={8}>
-          <p>{detailed.projectsubtype ? detailed.projectsubtype : ''}</p>
+          <p>{detailed.projectsubtype ? detailed.projectsubtype : '-'}</p>
         </Col>
         </>}
       </Row>
-      <Row>
-      { detailed.projectsubtype !== 'Debris Management' ? <>
-        <Col span={4}>
-          <label><i>Completed Year</i></label>
-        </Col>
-        <Col span={8}>
-          <p>{detailed.completedyear}</p>
-        </Col>
-        </> : <>
+      
+      { detailed.projecttype === 'Maintenance' && <Row>
         <Col span={4}>
           <label><i>Project Status</i></label>
         </Col>
         <Col span={8}>
           <p>{detailed.status ? detailed.status : ''}</p>
         </Col>
-        </>}
-        {}
-        <Col span={4}>
-          <label><i>LG Manager</i></label>
-        </Col>
-        <Col span={8}>
-          <p>{detailed.lgmanager ? detailed.lgmanager : ''}</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={4}>
-          <label><i>MHFD Manager</i></label>
-        </Col>
-        <Col span={8}>
-          <p>{detailed.mhfdmanager ? detailed.mhfdmanager : ''}</p>
-        </Col>
-        { detailed.projectsubtype === 'Debris Management' && <>
         <Col span={4}>
           <label><i>Frequency</i></label>
         </Col>
         <Col span={8}>
-          <p>{detailed.frequency ? detailed.frequency + ' times per year' : ''}</p>
+          <p>{detailed.frequency ? detailed.frequency + ' times per year' : '-'}</p>
         </Col>
-        </>}
+      </Row>}
+      {(detailed.projecttype === 'Maintenance' && detailed.projectsubtype !== 'Debris Management') && <Row>
+        <Col span={4}>
+          <label><i>Completed Year</i></label>
+        </Col>
+        <Col span={20}>
+          <p>{detailed.completedyear ? detailed.completedyear : '-'}</p>
+        </Col>
       </Row>
-      <Row>
+      }
+      { detailed.projecttype === 'Capital' && <Row>
+        <Col span={4}>
+          <label><i>Completed Year</i></label>
+        </Col>
+        <Col span={8}>
+          <p>{detailed.completedyear ? detailed.completedyear : '-'}</p>
+        </Col>
+        <Col span={4}>
+          <label><i>LG Manager</i></label>
+        </Col>
+        <Col span={8}>
+          <p>{detailed.lgmanager ? detailed.lgmanager : '-'}</p>
+        </Col>
+      </Row>}
+      {detailed.projecttype === 'Capital' && <Row>
+        <Col span={4}>
+          <label><i>MHFD Manager</i></label>
+        </Col>
+        <Col span={20}>
+          <p>{detailed.mhfdmanager ? detailed.mhfdmanager : '-'}</p>
+        </Col>
+      </Row>}
+      {detailed.problemtype || (detailed.projecttype === 'Capital') && <Row>
         <Col span={4}>
           <label><i>Description</i></label>
         </Col>
         <Col span={20}>
-          <p>{detailed.description ? detailed.description : ''}</p>
+          <p>{detailed.description ? detailed.description : '-'}</p>
         </Col>
       </Row>
+      }
+      {detailed.projecttype === 'Study' && <Row>
+        <Col span={4}>
+          <label><i>Tributary A</i></label>
+        </Col>
+        <Col span={8}>
+          <p>-</p>
+        </Col>
+        <Col span={4}>
+          <label><i>Tributary B</i></label>
+        </Col>
+        <Col span={8}>
+          <p>-</p>
+        </Col>
+      </Row> }
+      {detailed.projecttype === 'Study' && <Row>
+        <Col span={4}>
+          <label><i>Start Year</i></label>
+        </Col>
+        <Col span={8}>
+          <p>{detailed.startyear ? detailed.startyear : ''}</p>
+        </Col>
+        <Col span={4}>
+          <label><i>Completed Year</i></label>
+        </Col>
+        <Col span={8}>
+          <p>{detailed.completedyear ? detailed.completedyear : ''}</p>
+        </Col>
+      </Row> }
     </>}
   </div>
 }
