a777b0d6ae4408eb549b832af3b35f5125832edb
Lua <xdaddisxd@gmail.com>
2020-07-31 12:44:49 -0400

    fema layers

--- src/constants/mapStyles.ts
+++ src/constants/mapStyles.ts
@@ -1543,18 +1543,66 @@ export const tileStyles = {
     type: 'fill',
     'source-layer': 'pluto15v1',
     layout: {},
-    paint: {
-        'fill-color': '#f7b532',
-        'fill-opacity': 0.7
+    "paint": {
+      "fill-color": "hsl(0, 97%, 13%)",
+      "fill-opacity": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          0,
+          1,
+          13,
+          0,
+          22,
+          0
+      ]
     }
   }, {
     type: 'line',
     'source-layer': 'pluto15v1',
     layout: {},
-    paint: {
-        'line-color': '#915e10',
-        'line-width': 0.5,
-        'line-opacity': 0.5
+    "paint": {
+      "line-color": "hsl(0, 95%, 20%)",
+      "line-width": 6,
+      "line-blur": 5,
+      "line-opacity": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          0,
+          1,
+          13,
+          1,
+          22,
+          0
+      ]
+    }
+  }, {
+    type: 'line',
+    'source-layer': 'pluto15v1',
+    layout: {},
+    "paint": {
+      "line-color": "hsl(0, 63%, 97%)",
+      "line-dasharray": [1, 1],
+      "line-width": 1.5
+    }
+  }, {
+    type: 'symbol',
+    'source-layer': 'pluto15v1',
+    "layout": {
+      "text-field": [
+          "step",
+          ["zoom"],
+          ["to-string", ["get", "fld_zone"]],
+          0,
+          ["to-string", ["get", "fld_zone"]],
+          22,
+          ["to-string", ["get", "fld_zone"]]
+      ]
+    },
+    "paint": {
+        "text-color": "hsl(0, 4%, 98%)",
+        "text-opacity": ["step", ["zoom"], 0, 14, 1, 22, 1]
     }
   }],
   [FLOODPLAINS_NON_FEMA_FILTERS]: [{
