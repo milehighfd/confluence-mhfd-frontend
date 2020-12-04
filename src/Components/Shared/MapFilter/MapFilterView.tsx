fe4c6198c371f1b5284c1a46099c6cdcd38a5708
Rosba <rosalbita15@gmail.com>
2020-12-04 10:21:16 -0400

    edit button modal profil, popover filters and loader

--- src/Components/Shared/MapFilter/MapFilterView.tsx
+++ src/Components/Shared/MapFilter/MapFilterView.tsx
@@ -346,7 +346,7 @@ export default ({ selectCheckboxes, setVisibleDropdown, selectedLayers, setSelec
         }
         removePopup();
         setVisibleDropdown(false);
-      }}>Clear</Button>
+      }}>Close</Button>
 
     </div>
   </div>
