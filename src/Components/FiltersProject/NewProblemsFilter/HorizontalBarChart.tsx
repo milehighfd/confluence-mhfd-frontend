1d818dd79d1bc9c66c51b2cf3c1eb6523890b61b
Bizonomy <bizonomy@gmail.com>
2020-12-21 23:40:14 -0400

    add conditional for horizontal bar chart

--- src/Components/FiltersProject/NewProblemsFilter/HorizontalBarChart.tsx
+++ src/Components/FiltersProject/NewProblemsFilter/HorizontalBarChart.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useRef } from 'react';
 
 import * as d3 from 'd3';
 
-const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
+const HorizontalBarChart = ({ data, type, selected, onSelect }: any) => {
+  console.log('data', data);
+  console.log('selected', selected);
   const svgRef = useRef<SVGSVGElement>(null);
 
   const labelmap: any = {
@@ -15,17 +17,28 @@ const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
   let selectedData = selected.split(',').filter((r: any) => r !== '').map((r: any) => +r);
 
   useEffect(() => {
-    data = data.map((r: any) => {
-      let index = selectedData.indexOf(r.value)
-      return {
-        value: r.value,
-        count: r.count,
-        selected: index === -1 ? false : true
-      }
-    })
+    if (type === 'solutionstatus') {
+      data = data.map((r: any) => {
+        let index = selectedData.indexOf(r.value)
+        return {
+          value: r.value,
+          count: r.count,
+          selected: index === -1 ? false : true
+        }
+      })
+    } else if (type === 'status') {
+      data = data.map((r: any) => {
+        let index = selectedData.indexOf(r.value)
+        return {
+          value: r.value,
+          count: r.counter,
+          selected: index === -1 ? false : true
+        }
+      })
+    }
 
     const width = 200;
-    const height = 200;
+    const height = type === 'solutionstatus' ? 200 : 300;;
 
     d3.select(svgRef.current).select('g').remove();
 
@@ -34,7 +47,7 @@ const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
       .attr("height", height)
       .append("g")
 
-    let maxi: any = d3.max(data, (d:any) => d.count);
+    let maxi: any = d3.max(data, (d: any) => d.count);
 
     var x = d3.scaleLinear()
       .domain([0, maxi])
@@ -44,27 +57,33 @@ const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x))
       .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
-  
-    var y:any = d3.scaleBand()
-      .range([10, height ])
-      .domain(data.map(function(d:any) { return d.value; }))
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    var y: any = d3.scaleBand()
+      .range([10, height])
+      .domain(data.map(function (d: any) { return d.value; }))
       .padding(.1);
     svg.append("g")
       .call(d3.axisLeft(y))
-  
-    var ydr:any = (d:any) => { return y(d.value)+10 };
+
+    var ydr: any = (d: any) => { return y(d.value) + 10 };
     svg.selectAll("myRect")
       .data(data)
       .enter()
       .append("rect")
-      .attr("x", x(0) )
+      .attr("x", x(0))
       .attr("y", ydr)
-      .attr("width", function(d:any) { return x(d.count); })
-      .attr("height", 25 )
+      .attr("width", function (d: any) { return x(d.count); })
+      .attr("height", function (d: any) {
+        if (type === 'solutionstatus') {
+          return 25;
+        } else if (type === 'status') {
+          return y.bandwidth();
+        }
+      })
       .attr("fill", "#261964")
-      .style("opacity", function(d:any) {
+      .style("opacity", function (d: any) {
         let index = selectedData.indexOf(d.value);
         if (index !== -1) {
           return 1;
@@ -88,9 +107,15 @@ const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
       .data(data)
       .enter()
       .append('text')
-      .text(function(d:any){ return labelmap[d.value] })
-      .attr("transform", function(d:any) {
-        let xo = 10 ;
+      .text(function (d: any) {
+        if (type === 'solutionstatus') {
+          return labelmap[d.value]
+        } else if (type === 'status') {
+          return d.value;
+        }
+      })
+      .attr("transform", function (d: any) {
+        let xo = 10;
         let yo = ydr(d) - 5;
         return `translate(${xo}, ${yo})`;
       })
@@ -101,9 +126,9 @@ const HorizontalBarChart = ({ data, selected, onSelect }: any) => {
       .data(data)
       .enter()
       .append('text')
-      .text(function(d:any){ return d.count })
-      .attr("transform", function(d:any) {
-        let xo = x(d.count) - (d.count < 10 ? 10 : 20);
+      .text(function (d: any) { return d.count })
+      .attr("transform", function (d: any) {
+        let xo = x(d.count) - (d.count < 10 ? 10 : (d.count < 100 ? 20 : 30));
         let yo = ydr(d) + 15;
         return `translate(${xo}, ${yo})`;
       })
