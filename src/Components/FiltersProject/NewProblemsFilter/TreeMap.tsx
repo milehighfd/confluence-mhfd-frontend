fbd0f9f8e9565bc68452ffa181175069ead033bf
Bizonomy <bizonomy@gmail.com>
2020-12-28 01:19:01 -0400

    Adjust TreeMap for filter dependence

--- src/Components/FiltersProject/NewProblemsFilter/TreeMap.tsx
+++ src/Components/FiltersProject/NewProblemsFilter/TreeMap.tsx
@@ -1,11 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import * as d3 from 'd3';
+import { Button } from 'antd';
 
-const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
+const TreeMap = ({ data, type, tab, selected, onSelect, defaultValue }: any) => {
   const svgRef = useRef<SVGSVGElement>(null);
 
-  let selectedData = selected === '' ? [] : selected.split(',')
+  const [selectedData, setSelectedData] = useState<string[]>([]);
+
+  useEffect(() => {
+    let sData = selected === '' ? [] : selected.split(',')
+    setSelectedData(sData);
+  }, [selected])
 
   let color: string;
 
@@ -37,16 +43,16 @@ const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
   } else {
     let sum = 0;
     (data || []).forEach((d: any) => {
-      sum += d.counter + 1;
+      sum += d.counter;
     });
     data = {
       name: '',
       children: (data || []).map((d: any) => {
         return {
           name: d.value,
-          value: d.counter + 1,
+          value: d.counter,
           colname: 'level2',
-          percentage: (d.counter + 1) / sum
+          percentage: (d.counter) / sum
         }
       })
     }
@@ -89,25 +95,16 @@ const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
         if (index !== -1) {
           return 1;
         } else {
-          return 0.7;
+          return 0.6;
         }
       })
     
     rects.on('click', (d: any) => {
-      console.log('click tree d', d);
       let index = selectedData.indexOf(d.data.name);
       if (index !== -1) {
-        selectedData.splice(index, 1);
+        setSelectedData(selectedData.filter((_, ind) => ind !== index))
       } else {
-        selectedData.push(d.data.name);
-      }
-      selectedData = selectedData.map((r: any) => `${r}`)
-      if (type === 'servicearea') {
-        onSelect(selectedData.join(','))
-      } else if (type === 'county' && tab === 'problem') {
-        onSelect(selectedData)
-      } else if (type === 'county' && tab === 'project') {
-        onSelect(selectedData.join(','))
+        setSelectedData([...selectedData, d.data.name])
       }
     })
 
@@ -128,19 +125,17 @@ const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
         if (index !== -1) {
           return 1;
         } else {
-          return 0.7;
+          return 0.6;
         }
       })
 
     newRects.on('click', (d: any) => {
       let index = selectedData.indexOf(d.data.name);
       if (index !== -1) {
-        selectedData.splice(index, 1);
+        setSelectedData(selectedData.filter((_, ind) => ind !== index))
       } else {
-        selectedData.push(d.data.name);
+        setSelectedData([...selectedData, d.data.name])
       }
-      selectedData = selectedData.map((r: any) => `${r}`)
-      onSelect(selectedData)
     })
 
     var textsOffsetX = 0;
@@ -171,7 +166,14 @@ const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
       .transition().duration(2000)
       .attr("x", function (d: any) { return (d.x0 + d.x1) / 2 + textsOffsetX })
       .attr("y", function (d: any) { return (d.y1 + d.y0) / 2 + textsOffsetY })
-      .text(function (d: any) { return d.data.name })
+      .text(function (d: any) {
+        console.log('d.data.percentage', d.data.percentage, d.data.percentage > 10, typeof d.data.percentage)
+        if (d.data.percentage > 0.10) {
+          return d.data.name;
+        } else {
+          return '';
+        }
+      })
       .attr("font-size", "10px")
       .attr("fill", "black")
       .style("text-anchor", "middle")
@@ -204,15 +206,45 @@ const TreeMap = ({ data, type, tab, selected, onSelect }: any) => {
       .transition().duration(2000)
       .attr("x", function (d: any) { return (d.x0 + d.x1) / 2 + percentageOffsetX })
       .attr("y", function (d: any) { return (d.y1 + d.y0) / 2 + percentageOffsetY })
-      .text(function (d: any) { return d3.format('.2%')(d.data.percentage) })
+      .text(function (d: any) {
+        if (d.data.percentage > 0.10) {
+          return d.data.value;
+        } else {
+          return '';
+        }
+      })
       .attr("font-size", "10px")
       .attr("fill", "black")
       .style("text-anchor", "middle")
 
   }, [data])
 
+  const apply = () => {
+    if (type === 'servicearea') {
+      onSelect(selectedData.join(','))
+    } else if (type === 'county' && tab === 'problem') {
+      onSelect(selectedData)
+    } else if (type === 'county' && tab === 'project') {
+      onSelect(selectedData.join(','))
+    } else if (type === 'county' && tab === 'component') {
+      onSelect(selectedData.join(','))
+    }
+  }
+
+  const reset = () => {
+    onSelect(defaultValue);
+  }
+
   return (
-    <svg ref={svgRef} />
+    <>
+      <svg ref={svgRef} />
+      <Button onClick={apply}>
+        apply
+      </Button>
+      <Button onClick={reset}>
+        reset
+      </Button>
+    </>
   )
 }
 
