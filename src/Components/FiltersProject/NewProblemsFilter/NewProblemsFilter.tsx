752a3abb07895548e62c405e3d416d663ac2041a
Bizonomy <bizonomy@gmail.com>
2020-12-31 02:18:29 -0400

    update NewProblems Layout

--- src/Components/FiltersProject/NewProblemsFilter/NewProblemsFilter.tsx
+++ src/Components/FiltersProject/NewProblemsFilter/NewProblemsFilter.tsx
@@ -5,27 +5,20 @@ import RheoStat from './RheoStat';
 import BarChart from './BarChart';
 import TreeMap from './TreeMap';
 import HorizontalBarChart from './HorizontalBarChart';
+import { useMapDispatch, useMapState } from '../../../hook/mapHook';
 const { Option } = Select;
 const content = (<div className="popoveer-00"><b>Solution Cost:</b> is the total estimated cost to solve a problem</div>);
 const content01 = (<div className="popoveer-00"><b>Priority:</b> is the severity of a problem relative to other problems of the same type.</div>);
-const content02 = (<div className="popoveer-00"><b>Element Type:</b> describes the type of improvements needed to solve a Problem.</div>);
-const content03 = (<div className="popoveer-00"><b>Status:</b> is the percentage (by cost) of elements required to solve a problem that have been completed.</div>);
-const content04 = (<div className="popoveer-00"><b>Source</b> is the document or process through which a Problem was identified.</div>);
-const content05 = (<div className="popoveer-00"><b>Total Cost:</b> is the Estimated Cost (for Projects in progress) or Final Cost (for completed Projects).</div>);
-const content06 = (<div className="popoveer-00"><b>Project Status:</b> is the current status of the Project. Some statuses are only applicable to certain project types.</div>);
-const content07 = (<div className="popoveer-00"><b>Start Year:</b> is the year a Project was initiated. For Projects that have not been initiated, use the "Work Plan Year" filter.</div>);
-// const content08 = (<div className="popoveer-00"><b>Completed Year:</b> represents the year a Project was finished (monitoring may still be occurring).</div>);
-const content09 = (<div className="popoveer-00"><b>MHFD Dollars Allocated:</b> is the amount of funding that MHFD has budgeted or encumbered for a particular Project. For Capital projects and Master Plans, this is the number that must at least be matched by a local government.</div>);
-const content10 = (<div className="popoveer-00"><b>Work Plan Year:</b> is the year that a proposed Project is on the approved MHFD Work Plan.</div>);
-const content11 = (<div className="popoveer-00"><b>Problem Type:</b> is the type of Problem that a Project is intended to help solve.</div>);
-const content12 = (<div className="popoveer-00"><b>Local Government Manager:</b> is the staff person at a local government responsible for planning or implementation of a Project.</div>);
-const content13 = (<div className="popoveer-00"><b>Creator:</b> is the Confluence user who first created a Project in the Confluence database.</div>);
-const content14 = (<div className="popoveer-00"><b>Component Type:</b> is a description of the type of Improvement or Data Point that has been identified at a particular location. (The term "Component" refers to a "Component of the Solution to a Problem," in the context of Capital Projects, or to a "Component of a Problem," in the context of Maintenance Projects.)</div>);
-const content15 = (<div className="popoveer-00"><b>Component Status:</b> is the status of implementing an improvement. (The term "Component" refers to a "Component of the Solution to a Problem," in the context of Capital Projects, or to a "Component of a Problem," in the context of Maintenance Projects.)</div>);
-const content16 = (<div className="popoveer-00"><b>Year of Study:</b> refers to the year of the Study in which the Component was first identified or proposed.</div>);
-const content17 = (<div className="popoveer-00"><b>Estimated Cost:</b> is the Estimated Cost of implementing or addressing a Component as part of a Capital or Maintenance project.</div>);
+const content02 = (<div className="popoveer-00"><b>Status:</b> is the percentage (by cost) of elements required to solve a problem that have been completed.</div>);
+const content03 = (<div className="popoveer-00"><b>Problem Type:</b> is the type of Problem that a Project is intended to help solve.</div>);
+const content04 = (<div className="popoveer-00"><b>Service Area:</b> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos blanditiis, sit omnis rerum nam, officia tempore sunt pariatur nihil deserunt non enim! Eligendi iure repellendus natus dolore temporibus quidem numquam.</div>);
+const content05 = (<div className="popoveer-00"><b>County:</b> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos blanditiis, sit omnis rerum nam, officia tempore sunt pariatur nihil deserunt non enim! Eligendi iure repellendus natus dolore temporibus quidem numquam.</div>);
+const content06 = (<div className="popoveer-00"><b>Jurisdiction:</b> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos blanditiis, sit omnis rerum nam, officia tempore sunt pariatur nihil deserunt non enim! Eligendi iure repellendus natus dolore temporibus quidem numquam.</div>);
+const content07 = (<div className="popoveer-00"><b>MHFD Project Manager:</b> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos blanditiis, sit omnis rerum nam, officia tempore sunt pariatur nihil deserunt non enim! Eligendi iure repellendus natus dolore temporibus quidem numquam.</div>);
 
 export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilterProblemOptions, getGalleryProblems }: any) => {
+    const { getParamFilterProblems } = useMapDispatch();
+    const { boundsMap } = useMapState();
 
     const apply = (values: any, field: string) => {
         const options = { ...filterProblemOptions };
@@ -41,6 +34,7 @@ export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilt
         }
         setFilterProblemOptions(options);
         getGalleryProblems();
+        getParamFilterProblems(boundsMap, options);
     }
     const reset = () => {
         const options = { ...filterProblemOptions };
@@ -56,16 +50,19 @@ export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilt
         options.servicearea = '';
         setFilterProblemOptions(options);
         getGalleryProblems();
+        getParamFilterProblems(boundsMap, options);
     }
+
+    const axisLabel = 'Number of Problems';
 
     return (
         <>  <div className="scroll-filters" style={{ height: window.innerHeight - 280 }}>
             <Row className="filt-00" style={{ marginTop: '10px' }}>
                 <Col span={12}>
-                    <h5>Problem Type <Popover content={content11}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
+                    <h5>Problem Type <Popover content={content03}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.problemtype &&
-                        <PieChart type={'problemtype'}
+                        <PieChart type={'problemtype'} defaultValue={''}
                             data={paramProblems.problemtype}
                             selected={filterProblemOptions.problemtype}
                             onSelect={(e: string) => apply(e, 'problemtype')} />
@@ -75,19 +72,23 @@ export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilt
                     <h5>Solution Cost <Popover content={content}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.cost &&
-                        <RheoStat data={paramProblems.cost} selected={filterProblemOptions.cost} onSelect={(items: string) => {
-                            apply(items, 'cost');
-                        }} />
+                        <RheoStat
+                            defaultValue={[]} axisLabel={axisLabel}
+                            data={paramProblems.cost}
+                            selected={filterProblemOptions.cost} onSelect={(items: string) => {
+                                apply(items, 'cost');
+                            }}
+                        />
                     }
                 </Col>
             </Row>
 
             <Row className="filt-00">
                 <Col span={12}>
-                    <h5>Solution Status <Popover content={content03}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
+                    <h5>Solution Status <Popover content={content02}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.solutionstatus &&
-                        <HorizontalBarChart type={'solutionstatus'}
+                        <HorizontalBarChart type={'solutionstatus'} defaultValue={''} axisLabel={axisLabel}
                             data={paramProblems.solutionstatus} color={'#261964'}
                             selected={filterProblemOptions.solutionstatus}
                             onSelect={(items: any) => apply(items, 'solutionstatus')} />
@@ -97,25 +98,28 @@ export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilt
                     <h5>Priority <Popover content={content01}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.priority &&
-                        <BarChart data={paramProblems.priority} selected={filterProblemOptions.priority} onSelect={(items: any) => {
-                            apply(items, 'priority');
-                        }} />
+                        <BarChart data={paramProblems.priority}
+                            defaultValue={''} axisLabel={axisLabel}
+                            selected={filterProblemOptions.priority}
+                            onSelect={(items: any) => {
+                                apply(items, 'priority');
+                            }} />
                     }
                 </Col>
             </Row>
 
             <Row className="filt-00">
                 <Col span={12}>
-                    <h5>Service Area</h5>
+                    <h5>Service Area <Popover content={content04}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.servicearea &&
-                        <TreeMap data={paramProblems.servicearea} type={'servicearea'} tab={'problem'}
+                        <TreeMap data={paramProblems.servicearea} type={'servicearea'} tab={'project'}
                             selected={filterProblemOptions.servicearea} defaultValue={''}
                             onSelect={(e: string) => apply(e, 'servicearea')} />
                     }
                 </Col>
                 <Col span={12}>
-                    <h5>County <img src="/Icons/icon-19.svg" alt="" /></h5>
+                    <h5>County <Popover content={content05}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
                     {
                         paramProblems.county &&
                         <TreeMap data={paramProblems.county} type={'county'} tab={'problem'}
@@ -125,48 +129,48 @@ export const NewProblemsFilter = ({ paramProblems, filterProblemOptions, setFilt
                 </Col>
             </Row>
 
-            <h5 className="filt-h5">Additional filters</h5>
             <Row className="filt-00" gutter={[24, 16]}>
                 <Col span={12}>
-                    <label>Jurisdiction </label>
-                    <Select placeholder="- Select -" value={filterProblemOptions.jurisdiction ? filterProblemOptions.jurisdiction : '- Select -'}
-                        style={{ width: '100%' }} onChange={(e: string) => {
-                            apply(e, 'jurisdiction');
-                        }}>
-                        {(paramProblems.jurisdiction || []).map((element: string, index: number) => {
-                            return <Option key={index} value={element}>{element}</Option>
-                        })}
-                    </Select>
+                    <h5>Jurisdiction <Popover content={content06}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
+                    <>
+                        <Button className="btn-svg" onClick={() => { }}>
+                            <u>Apply</u>
+                        </Button>
+                    &nbsp;|&nbsp;
+                    <Button className="btn-svg" onClick={() => { apply('', 'jurisdiction') }}>
+                            <u>Reset</u>
+                        </Button>
+                        <Select placeholder="- Select -" value={filterProblemOptions.jurisdiction ? filterProblemOptions.jurisdiction : '- Select -'}
+                            style={{ width: '100%' }} onChange={(e: string) => {
+                                apply(e, 'jurisdiction');
+                            }}>
+                            {(paramProblems.jurisdiction || []).map((element: any, index: number) => {
+                                return element && <Option key={index} value={element.value}>{`${element.value} (${element.counter})`}</Option>
+                            })}
+                        </Select>
+                    </>
                 </Col>
                 <Col span={12}>
-                    <label>MHFD Watershed Manager</label>
-                    <Select placeholder="- Select -" value={filterProblemOptions.mhfdmanager ? filterProblemOptions.mhfdmanager : '- Select -'}
-                        style={{ width: '100%' }} onChange={(e: string) => {
-                            apply(e, 'mhfdmanager');
-                        }}>
-                        {(paramProblems.mhfdmanager || []).map((element: string, index: number) => {
-                            return <Option key={index} value={element}>{element}</Option>
-                        })}
-                    </Select>
+                    <h5>MHFD Project Manager <Popover content={content07}><img src="/Icons/icon-19.svg" alt="" /></Popover></h5>
+                    <>
+                        <Button className="btn-svg" onClick={() => { }}>
+                            <u>Apply</u>
+                        </Button>
+                    &nbsp;|&nbsp;
+                    <Button className="btn-svg" onClick={() => { apply('', 'mhfdmanager') }}>
+                            <u>Reset</u>
+                        </Button>
+                        <Select placeholder="- Select -" value={filterProblemOptions.mhfdmanager ? filterProblemOptions.mhfdmanager : '- Select -'}
+                            style={{ width: '100%' }} onChange={(e: string) => {
+                                apply(e, 'mhfdmanager');
+                            }}>
+                            {(paramProblems.mhfdmanager || []).map((element: any, index: number) => {
+                                return element && <Option key={index} value={element.value}>{`${element.value} (${element.counter})`}</Option>
+                            })}
+                        </Select>
+                    </>
                 </Col>
             </Row>
-            {/* <Row className="filt-00" gutter={[24, 16]}>
-                <Col span={12}>
-                    <label>Source <Popover content={content04}><img src="/Icons/icon-19.svg" alt="" width="12px" /></Popover></label>
-                    <Select placeholder="- Select -" value={filterProblemOptions.source ? filterProblemOptions.source : '- Select -'}
-                        style={{ width: '100%' }} onChange={(e: string) => {
-                            apply(e, 'source');
-                        }}>
-                        {(paramProblems.source || []).map((element: string, index: number) => {
-                            return <Option key={index} value={element}>{element}</Option>
-                        })}
-                    </Select>
-                </Col>
-            </Row> */}
-
-            <div className="btn-footer" style={{ marginTop: '25px' }}>
-                <Button onClick={() => reset()} style={{ width: '140px' }} className="btn-borde">Reset</Button>
-            </div>
         </div>
         </>
     )
