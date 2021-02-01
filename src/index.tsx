8e27f58efb63abd8915f3e6d1122ce6782ee47a4
Lua <xdaddisxd@gmail.com>
2021-02-01 08:54:27 -0400

    add force https library

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,8 @@ import {history} from "./store/configureStore";
 import store from './store';
 import 'antd/dist/antd.css';
 import '@mdi/font/scss/materialdesignicons.scss';
+import HttpsRedirect from 'react-https-redirect';
+
 
 import App from './AppContainer';
 
@@ -33,11 +35,15 @@ export const initGA = () => {
 };
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>, document.getElementById('root'));
+  <HttpsRedirect>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+    </HttpsRedirect>, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,4 @@
 declare module 'd3-simple-slider';
 declare module '@deck.gl/mapbox';
 declare module '@deck.gl/layers';
+declare module 'react-https-redirect';
