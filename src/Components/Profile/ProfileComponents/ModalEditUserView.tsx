31ae7074dbd8cb40e88bbd4e7ac41bf24c12e2cc
Lua <xdaddisxd@gmail.com>
2021-02-05 15:58:55 -0400

    disable dd of organization on goverment staff

--- src/Components/Profile/ProfileComponents/ModalEditUserView.tsx
+++ src/Components/Profile/ProfileComponents/ModalEditUserView.tsx
@@ -209,7 +209,7 @@ export default ({ user, updateUserInformation, isVisible, hideProfile, groupOrga
             <Col className="gutter-row" span={12}>
               <p>ORGANIZATION</p>
               {values.designation !== OTHER ? <div id="sign-up-organization">
-                <Dropdown className={values.designation === ADMIN ? '' : ''} disabled={values.designation === ADMIN} overlay={menu} getPopupContainer={() => document.getElementById("sign-up-organization") as HTMLElement}>
+                <Dropdown disabled={values.designation === GOVERNMENT_STAFF} overlay={menu} getPopupContainer={() => document.getElementById("sign-up-organization") as HTMLElement}>
                   <Button style={{ paddingLeft: '10px' }} className="btn-borde" >
                     {values.organization ? values.organization : ((values.designation === GOVERNMENT_ADMIN || values.designation === GOVERNMENT_STAFF) ? 'Local government' : 'Organization')}
                     <img src="/Icons/icon-12.svg" alt="" />
