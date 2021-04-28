26108166d3c9707d7f0599e2b5d708a0aa9960a1
Rosba <rosalbita15@gmail.com>
2021-04-28 14:40:31 -0400

    edit select in capityal project

--- src/Components/Project/TypeProjectComponents/LocationInformation.tsx
+++ src/Components/Project/TypeProjectComponents/LocationInformation.tsx
@@ -20,7 +20,7 @@ export const LocationInformation = ({setServiceArea, setCounty, setJurisdiccion,
   const [sArea, setSArea] = useState(undefined);
   const [sCounty, setSCounty] = useState(undefined);
   const [disable , setdisable ] = useState(!editable);
-  
+
   const apllyServiceArea = (e: any)=>{
     setServiceArea(e);
     setSArea(e);
@@ -50,34 +50,40 @@ export const LocationInformation = ({setServiceArea, setCounty, setJurisdiccion,
   },[currentServiceAreaCounty]);
   return(
     <>
-    <h5>4. Location Information <Popover content={content08}><img src="/Icons/icon-19.svg" alt="" height="14px" /></Popover></h5>    
+    <h5>4. Location Information <Popover content={content08}><img src="/Icons/icon-19.svg" alt="" height="14px" /></Popover></h5>
     <Row gutter={[16, 16]}>
       <Col xs={{ span: 24 }} lg={{ span: 12 }}>
         <label className="sub-title">Service Area <Popover content={content01}><img src="/Icons/icon-19.svg" alt="" height="10px" /></Popover></label>
-        <Select mode="multiple" placeholder={serviceArea.length!=0?serviceArea:"Select a Service Area"} style={{width:'100%'}} onChange={(serviceArea:any)=> setServiceArea(serviceArea)} value={serviceArea}>
-          {SERVICE_AREA.map((element) =>{
-            if(element!= 'None'){
-              if(element != 'Boulder Service Area'){
-                return <Option key={element} value={element}>{element}</Option>
-              }
-            }})}
-        </Select>
+        <div className="sponsor-select">
+          <Select mode="multiple" placeholder={serviceArea.length!=0?serviceArea:"Select a Service Area"} style={{width:'100%'}} onChange={(serviceArea:any)=> setServiceArea(serviceArea)} value={serviceArea}>
+            {SERVICE_AREA.map((element) =>{
+              if(element!= 'None'){
+                if(element != 'Boulder Service Area'){
+                  return <Option key={element} value={element}>{element}</Option>
+                }
+              }})}
+          </Select>
+        </div>
       </Col>
       <Col xs={{ span: 24 }} lg={{ span: 12 }}>
         <label className="sub-title">County <Popover content={content02}><img src="/Icons/icon-19.svg" alt="" height="10px" /></Popover></label>
-        <Select mode="multiple" placeholder={county.length!=0?county:"Select a County"} style={{width:'100%'}} value={county} onChange={(county:any)=> apllyCounty(county)} disabled={disable}>
-          {PROJECT_INFORMATION.COUNTRY_PROJECT.map((element) =>{
-            return <Option key={element} value={element}>{element}</Option>
-          })}
-        </Select>
+        <div className="sponsor-select">
+          <Select mode="multiple" placeholder={county.length!=0?county:"Select a County"} style={{width:'100%'}} value={county} onChange={(county:any)=> apllyCounty(county)} disabled={disable}>
+            {PROJECT_INFORMATION.COUNTRY_PROJECT.map((element) =>{
+              return <Option key={element} value={element}>{element}</Option>
+            })}
+          </Select>
+        </div>
       </Col>
       <Col xs={{ span: 24 }} lg={{ span: 12 }}>
         <label className="sub-title">Jurisdiccion <Popover content={content02}><img src="/Icons/icon-19.svg" alt="" height="10px" /></Popover></label>
-        <Select mode="multiple" placeholder={jurisdiccion.length!=0?jurisdiccion:"Select a Jurisdiccion"} style={{width:'100%'}} value={jurisdiccion} onChange={(jurisdiccion:any)=> setJurisdiccion(jurisdiccion)}>
-        {JURISDICTION.map((element:string ) =>{
-            return <Option key={element} value={element}>{element}</Option>
-          })}
-        </Select>
+        <div className="sponsor-select">
+          <Select mode="multiple" placeholder={jurisdiccion.length!=0?jurisdiccion:"Select a Jurisdiccion"} style={{width:'100%'}} value={jurisdiccion} onChange={(jurisdiccion:any)=> setJurisdiccion(jurisdiccion)}>
+          {JURISDICTION.map((element:string ) =>{
+              return <Option key={element} value={element}>{element}</Option>
+            })}
+          </Select>
+        </div>
       </Col>
     </Row>
     <br/>
