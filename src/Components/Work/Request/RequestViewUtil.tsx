8cb17c758fe0a1a04e289d82b7922ab3465b5a28
pachonjcl <pachonjcl@gmail.com>
2021-05-24 15:52:04 -0400

    add counting for non request projects

--- src/Components/Work/Request/RequestViewUtil.tsx
+++ src/Components/Work/Request/RequestViewUtil.tsx
@@ -412,7 +412,7 @@ export const getTotalsByProperty = (columns: any[], property: string) => {
       localityMap[locality].projects.push(p);
       for(var i = 1; i <= 5 ; i++) {
         localityMap[locality][`req${i}`] += (p[`req${i}`] / arr.length);
-        if (p[`req${i}`]) {
+        if (p.hasOwnProperty(`position${i}`)) {
           localityMap[locality][`cnt${i}`]++;
         }
       }
