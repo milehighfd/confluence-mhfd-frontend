992a8376d940a034a0cfcdf0ce031b3892a3dd57
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-03-16 12:53:33 -0400

    edit data popup in block Clearanze Zone

--- src/Components/WorkRequestMap/WorkRequestMap.tsx
+++ src/Components/WorkRequestMap/WorkRequestMap.tsx
@@ -1793,12 +1793,11 @@ const epochTransform = (dateParser: any) => {
       }
       if (feature.source === STREAM_MANAGEMENT_CORRIDORS) {
         const item = {
-          layer: MENU_OPTIONS.STREAM_MANAGEMENT_CORRIDORS,
-          scale: 'District',//feature.properties.scale,
+          layer: feature.properties.smc_type,
+          scale: feature.properties.scale,
           date_created: '01/07/2019' //feature.properties.date_created,
-        }
-        
-        menuOptions.push(MENU_OPTIONS.STREAM_MANAGEMENT_CORRIDORS);
+      }
+      menuOptions.push(feature.properties.smc_type);
         popups.push(item);
         mobile.push({
           layer: item.layer,
