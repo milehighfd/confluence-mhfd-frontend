fc5eca5e6121ac4a905e94746d53d55568d10d72
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-03-21 10:53:11 -0400

    edit button style in filter and sort

--- src/Components/Map/MapView.tsx
+++ src/Components/Map/MapView.tsx
@@ -816,7 +816,7 @@ const MapView = ({ filters, removeFilter, getDropdownFilters,
   const genExtra = () => (
     <Row type="flex" justify="space-around" align="middle" style={{ cursor: 'pointer' }}>
       <Col>
-        <div className={(spinFilter || spinCardProblems || spinCardProjects ||spinMapLoaded ) ? "apply-filter" : 'apply-filter-no-effect'}>
+        <div className={(spinFilter || spinCardProblems || spinCardProjects ||spinMapLoaded ) ? "apply-filter" : 'apply-filter-no-effect'} style={{ borderColor:'transparent' }}>
           Apply map view to filter
           <Checkbox style={{ paddingLeft: 6 }} checked={applyFilter} onChange={() => {
             setApplyFilter(!applyFilter)
@@ -998,8 +998,8 @@ const MapView = ({ filters, removeFilter, getDropdownFilters,
           <Col style={{ textAlign: 'right' }} span={13} id="sort-map">
             <Button className="btn-red" onClick={onResetClick}><u>Reset</u></Button>
             <Popover placement="bottomRight" overlayClassName="tag-filters" content={getFiltersPopoverContent()}>
-              <Button onClick={handleToggle} style={{ marginLeft:'22px', marginRight:'9px', borderRadius: '4px'}} className="btn-filter">
-                {/* <img style={{ background: backgroundStyle }} className="img-filter" alt="" /> */}
+              <Button onClick={handleToggle} style={{ marginLeft:'22px', marginRight:'9px', borderRadius: '4px', backgroundColor:'transparent', borderColor:'transparent'}} className="btn-filter">
+                <img style={{ background: backgroundStyle }} className="img-filter" alt="" />
                 <span style={{ color: textStyle, marginLeft:'-3px', fontFamily:'Ubuntu'}}> {filterLabel} </span>
                 <span className="circle">
                   <span className="innercircle">
@@ -1025,9 +1025,10 @@ const MapView = ({ filters, removeFilter, getDropdownFilters,
                   Sort by {tabActive === '0' ? SORTED_PROBLEMS.filter(element => element.name === filterProblemOptions.column)[0]?.title :
                     SORTED_PROJECTS.filter(element => element.name === filterProjectOptions.column)[0]?.title}
                 </span> */}
-                <Button onClick={sortClick} style={{ marginLeft:'15px', marginRight:'15px', borderRadius: '4px'}} className="btn-filter">
+                <Button onClick={sortClick} style={{ marginLeft:'15px', marginRight:'15px', borderRadius: '4px',  backgroundColor:'transparent', borderColor:'transparent'}} className="btn-filter">
+                  <img style={{  width:'20px' }} src={"Icons/ic_sort_by_active.svg"} />
                   <span style={{ color: textStyle, marginLeft:'-3px', fontFamily:'Ubuntu'}}> Sort By</span>
-                  <Icon type="down" className={'certain-category-icon ' + (filterProjectOptions.order !== 'asc' ? 'rotate-icon': 'normal-icon')} />
+                  {/* <Icon type="down" className={'certain-category-icon ' + (filterProjectOptions.order !== 'asc' ? 'rotate-icon': 'normal-icon')} /> */}
                 </Button>
               </Dropdown>
             </div>
