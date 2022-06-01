ea69da3ec7059fa742d1d56faec7172267e64622
jose <pachonjcl@gmail.com>
2022-06-01 12:44:56 -0400

    move autocomplete to its own component

--- src/routes/map/components/MapView.tsx
+++ src/routes/map/components/MapView.tsx
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Dropdown, Button, Tabs, Input, Menu, Popover, Checkbox, AutoComplete } from 'antd';
-import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Row, Col, Dropdown, Button, Tabs, Input, Menu, Popover, Checkbox } from 'antd';
+import { useLocation } from "react-router-dom";
 
 import GenericTabView from "../../../Components/Shared/GenericTab/GenericTabView";
 import FiltersProjectView from "../../../Components/FiltersProject/FiltersProjectView";
 
 import { FILTER_PROBLEMS_TRIGGER, FILTER_PROJECTS_TRIGGER, SORTED_PROBLEMS, SORTED_PROJECTS, PROBLEMS_TRIGGER, PROJECTS_TRIGGER, COMPONENTS_TRIGGER, SELECT_ALL_FILTERS } from '../../../constants/constants';
-import { useLocation } from "react-router-dom";
 import DetailedModal from "../../../Components/Shared/Modals/DetailedModal";
 import { useMapDispatch, useMapState } from "../../../hook/mapHook";
 import { capitalLetter, elementCost, getStatus } from '../../../utils/utils';
 import RheoStatService from '../../../Components/FiltersProject/NewProblemsFilter/RheoStatService';
 import { useProfileDispatch, useProfileState } from "../../../hook/profileHook";
 import { useDetailedState } from "../../../hook/detailedHook";
+import MapAutoComplete from "./MapAutoComplete";
 
 const tabs = [FILTER_PROBLEMS_TRIGGER, FILTER_PROJECTS_TRIGGER];
 let contents: any = [];
@@ -21,7 +21,6 @@ contents.push((<div className="popoveer-00"><b>Projects:</b> Projects are active
 
 const { TabPane } = Tabs;
 const { Search } = Input;
-const { Option } = AutoComplete;
 let counterZoomArea = 0 ;
 
 const MapView = () => {
@@ -72,10 +71,7 @@ const MapView = () => {
   } = useMapDispatch();
   const {getGroupOrganization} = useProfileDispatch();
   const { userInformation, groupOrganization } = useProfileState();
-  const {
-    designation,
-    zoomarea
-  } = userInformation;
+  const { zoomarea } = userInformation;
   const {
     tabCards,
     nameZoomArea,
@@ -94,12 +90,6 @@ const MapView = () => {
   const [countFilterComponents, setCountFilterComponents] = useState(0);
   const [countFilterProjects, setCountFilterProjects] = useState(0);
 
-  const [valueA, setvalueA] = useState('');
-  const [dropdownIsOpen, setDropdownIsOpen] = useState(false);
-
-  useEffect(() => {
-    setvalueA(nameZoomArea);
-  }, [nameZoomArea]);
   useEffect(() => {
     setSpinMapLoaded(true);
     getGroupOrganization();
@@ -653,51 +643,10 @@ const MapView = () => {
       }
     }
   }
-  const [dataAutocomplete, setDataAutocomplete] = useState(groupOrganization.filter(function (item: any) {
-    if (item.aoi === undefined) {
-      return false;
-    }
-    return true;
-  }).map((item: { aoi: string }) => { return <Option className="list-line" key={item.aoi}>{item.aoi}</Option> }));
 
-  useEffect(()=>{
-    setDataAutocomplete(groupOrganization.filter(function (item: any) {
-      if (item.aoi === undefined) {
-        return false;
-      }
-      return true;
-    }).map((item: { aoi: string }) => { return <Option className="list-line" key={item.aoi}>{item.aoi}</Option> }));
-  },[groupOrganization]);
-  const setValueInFilters = (value: any, type: any, filterOptions: any, withSuffix: boolean = false) => {
-    const options = { ...filterOptions };
-    options.jurisdiction = '';
-    options.county = '';
-    options.servicearea = '';
-    if (!withSuffix) {
-      if (value.includes('County')) {
-        let index = value.indexOf('County');
-        if (index !== -1) {
-          value = value.substr(0, index - 1);
-        }
-      }
-      if (value.includes('Service Area')) {
-        let index = value.indexOf('Service Area');
-        if (index !== -1) {
-          value = value.substr(0, index - 1);
-        }
-      }
-    }
-    if(type == "Service Area") {
-      options.servicearea = value;
-    } else if(type) {
-      options[type.toLowerCase()] = value;
-    }
-    return options;
-  }
   const onSelect = (value: any, isSelect?:any) => {
     console.log('Selected:', value, isSelect);
     setAutocomplete(value);
-    setvalueA(value);
     const zoomareaSelected = groupOrganization.filter((x: any) => x.aoi === value).map((element: any) => {
       return {
         aoi: element.aoi,
@@ -843,32 +792,9 @@ const MapView = () => {
         visible={visible}
         setVisible={setVisible}
       />}
-      <Row className="head-m mobile-display">
-        <Col span={24} id="westminter">
-          <div className="auto-complete-map">
-            <AutoComplete
-              style={{ width: '200' }}
-              onDropdownVisibleChange={setDropdownIsOpen}
-              dataSource={dataAutocomplete}
-              placeholder={nameZoomArea ? (nameZoomArea.endsWith(', CO') ? nameZoomArea.replace(', CO', '') : nameZoomArea) : 'Mile High Flood District'}
-              filterOption={(inputValue, option: any) => {
-                if (dataAutocomplete.map((r: any) => r.key).includes(inputValue)) {
-                  return true;
-                }
-                return option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1;
-              }}
-              onSelect={onSelect}
-              value={valueA}
-              onSearch={(input2: any) => {
-                setvalueA(input2)
-              }}
-            >
-              <Input id={'miclase'} style={{border: 'none', boxShadow: 'none', borderBottom: '1px solid rgba(37, 24, 99, 0.3)',marginRight: '-18px' }} suffix={dropdownIsOpen ? <UpOutlined style={{marginRight: '-18px'}}/> : <DownOutlined style={{marginRight: '-18px'}}/>} />
-            </AutoComplete>
-          </div>
-        </Col>
-      </Row>
-
+      <MapAutoComplete
+        onAutoCompleteSelected={onSelect}
+      />
       <div className="head-filter mobile-display">
         <Row justify="space-around" align="middle">
           <Col span={11}>
