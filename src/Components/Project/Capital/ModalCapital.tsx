2913e7dd466c24bcb36d4e5b3c5bb96aac1e8ae6
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-07-06 16:55:15 -0400

    add required in create projects

--- src/Components/Project/Capital/ModalCapital.tsx
+++ src/Components/Project/Capital/ModalCapital.tsx
@@ -627,7 +627,11 @@ export const ModalCapital = ({visibleCapital, setVisibleCapital, nameProject, se
               setDescription = {setDescription}
             />
             <br/>
-            <h5>2. SELECT COMPONENTS</h5>
+            <h5>
+              2. SELECT COMPONENTS
+              <span className="requiered">&nbsp;*&nbsp;</span>
+              <img src="/Icons/icon-08.svg" />
+            </h5>
 
             <div className={"draw "+(isDrawState?'active':'')} onClick={onClickDraw}>
               <img src="" className="icon-draw active" style={{WebkitMask: 'url("/Icons/icon-08.svg") center center no-repeat'}}/>
@@ -744,7 +748,7 @@ export const ModalCapital = ({visibleCapital, setVisibleCapital, nameProject, se
               }
             </Collapse>
             <Button className="btn-transparent-green" onClick={()=>{applyIndependentComponent()}}><PlusCircleFilled /> Independent Component</Button> <Popover content={contentIndComp}><img src="/Icons/icon-19.svg" alt="" height="10px" style={{marginBottom:'2px'}}/></Popover>
-            <h5>3. PROJECT GEOMETRY </h5>
+            <h5>3. PROJECT GEOMETRY<span className="requiered">&nbsp;*</span></h5>
 
             <div className={"draw "+(isDrawStateCapital?'active':'')}  onClick={onClickDrawCapital}>
               <img src="" className="icon-draw active" style={{WebkitMask: 'url("/Icons/icon-08.svg") center center no-repeat'}}/>
