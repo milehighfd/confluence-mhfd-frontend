b3d04d2760105f4202017aba75eda205ff511e29
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-10-24 12:08:52 -0400

    edit style porfolio

--- src/routes/portfolio-view/components/CalendarView.tsx
+++ src/routes/portfolio-view/components/CalendarView.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Button, Calendar, Col, Input, Layout, message, Popover, Row, Select, Space, Steps, Table, Tabs, Tag } from 'antd';
-import { CalendarOutlined, CloseOutlined, FormOutlined, ZoomInOutlined, ZoomOutOutlined } from "@ant-design/icons";
+import { Button, Calendar, Checkbox, Col, Input, Layout, message, Popover, Progress, Row, Select, Space, Steps, Table, Tabs, Tag } from 'antd';
+import { CalendarOutlined, ClockCircleOutlined, CloseOutlined, FormOutlined, ZoomInOutlined, ZoomOutOutlined } from "@ant-design/icons";
 import ModalTollgate from "routes/list-view/components/ModalTollgate";
 
 
@@ -24,58 +24,91 @@ const CalendarView = () => {
         <CloseOutlined onClick={()=>{setOpenPiney(false)}}/>
         <FormOutlined style={{fontSize:'20px'}}/>
       </div>
-      <h1 style={{color:'#000000', fontSize:'16px', marginBottom:'15px'}}>Piney Creek Channel Restore</h1>
-      <span className="tag-blue">Funding Phase</span><span className="tag-blue">Capital</span>
-      <p style={{marginTop:'20px', marginBottom:'5px', fontWeight:'700', opacity:'0.6'}}>Notes</p>
-      <p>The same screen can be built in a lot of different ways, but only a few of them will get your message accross correctly and result in an easy-to-use software or...<span style={{fontWeight:'700'}}>more</span></p>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>MHFD Lead</p>
-        </Col>
-        <Col xs={{ span: 10 }} lg={{ span: 12 }}>
-          <p>Jon Villines</p>
-        </Col>
-      </Row>
+      <div className="body-piney">
+        <h1 style={{color:'#000000', fontSize:'16px', marginBottom:'15px'}}>Piney Creek Channel Restore</h1>
+        <span className="tag-blue">Funding Phase</span><span className="tag-blue">Capital</span>
+        <p style={{marginTop:'20px', marginBottom:'5px', fontWeight:'700', opacity:'0.6'}}>Notes</p>
+        <p>The same screen can be built in a lot of different ways, but only a few of them will get your message accross correctly and result in an easy-to-use software or...<span style={{fontWeight:'700'}}>more</span></p>
+        <div className="form-text-calendar">
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>MHFD Lead</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+            <img src="/picture/user.png" alt="" height="24px" style={{borderRadius: '50%'}}/> <span>Jon Villines</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>Total Cost</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+              <p>$3,708,000</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>Phase</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+              <span>Funding</span> <span className="tag-blue">20%</span>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>Start Date</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+              <p>July 1, 2021</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>End Date</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+              <p>December 6, 2021</p>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 10 }}>
+              <p>Duration</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 14 }}>
+            <ClockCircleOutlined />&nbsp; &nbsp;  <span>5 months  5 days</span>
+            </Col>
+          </Row>
+        </div>
+        <p style={{marginTop:'10px', marginBottom:'5px', fontWeight:'700', opacity:'0.6'}}>Action Items</p>
+          <Row>
+            <Col xs={{ span: 10 }} lg={{ span: 4 }}>
+              <p style={{fontSize:'12px', fontWeight:'700', paddingTop:'2px'}}>20%</p>
+            </Col>
+            <Col xs={{ span: 10 }} lg={{ span: 20 }}>
+              <Progress percent={20} />
+            </Col>
+          </Row>
+        <div className="checkbox-select">
+          <p>Draft IGA</p>
+          <Checkbox></Checkbox>
+        </div>
+        <div className="checkbox-select">
+          <p>Sign IGA</p>
+          <Checkbox></Checkbox>
+        </div>
+        <div className="checkbox-select">
+          <p>Request Funding</p>
+          <Checkbox></Checkbox>
+        </div>
+        <div className="checkbox-select">
+          <p>Send Invoice</p>
+          <Checkbox></Checkbox>
+        </div>
+        <div className="checkbox-select">
+          <p>Pay Invoice</p>
+          <Checkbox></Checkbox>
+        </div>
+      </div>
     </div>}
     <Row style={{margin:'15px 10px'}}>
       <Col xs={{ span: 10 }} lg={{ span: 12 }}>
