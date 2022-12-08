0efe23d7e6d268f20d3eced5f0b533518af514cb
dottykurmi123@gmail.com <dottykurmi123@gmail.com>
2022-12-08 16:08:01 -0400

    edit style

--- src/routes/list-view/components/ModalTollgate.tsx
+++ src/routes/list-view/components/ModalTollgate.tsx
@@ -28,8 +28,17 @@ const ModalTollgate = ({visible, setVisible}: {visible: boolean, setVisible: Rea
         </Row>
         <Row className="detailed-h detailed-hh" gutter={[16, 16]} style={{backgroundColor: 'white', paddingTop:'0px', paddingBottom:'0px'}}>
           <Col xs={{ span: 12 }} lg={{ span: 24}}>
+            <Row>
+              <Col xs={{ span: 12 }} lg={{ span: 16}}>
+              </Col>
+              <Col xs={{ span: 12 }} lg={{ span: 8}} style={{textAlign:'center'}}>
+                <h3 style={{marginBottom:'0px'}}>Duration</h3>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={{ span: 12 }} lg={{ span: 24}}>
             <Row style={{height:'30px',overflowY: 'auto'}} className="row-modal-list-view">
-              <Col xs={{ span: 12 }} lg={{ span: 11}}>
+              <Col xs={{ span: 12 }} lg={{ span: 8}}>
                 <Row style={{height: '30px'}} >
                   <Col xs={{ span: 12 }} lg={{ span: 11}}>
                   </Col>
@@ -37,15 +46,25 @@ const ModalTollgate = ({visible, setVisible}: {visible: boolean, setVisible: Rea
                   </Col>
                 </Row>
               </Col>
-              <Col xs={{ span: 12 }} lg={{ span: 13}}>
+              <Col xs={{ span: 12 }} lg={{ span: 8}}>
                 <Row style={{height: '30px'}}>
                   <Col xs={{ span: 12 }} lg={{ span: 10}} style={{textAlign:'center'}}>
                     <h5>Start Date</h5>
                   </Col>
-                  <Col xs={{ span: 12 }} lg={{ span: 10}} style={{textAlign:'center'}}>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}} style={{textAlign:'center'}}>
                     <h5>End Date</h5>
                   </Col>
-                  <Col xs={{ span: 12 }} lg={{ span: 4}} style={{textAlign:'center'}}>
+                  <Col xs={{ span: 12 }} lg={{ span: 3}} style={{textAlign:'center'}}>
+                  </Col>
+                </Row>
+              </Col>
+              <Col xs={{ span: 12 }} lg={{ span: 8}}>
+                <Row style={{height: '30px'}}>
+                  <Col xs={{ span: 12 }} lg={{ span: 12}} style={{textAlign:'center'}}>
+                    <h5>Weeks</h5>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 12}} style={{textAlign:'center'}}>
+                    <h5>Days</h5>
                   </Col>
                 </Row>
               </Col>
@@ -55,7 +74,7 @@ const ModalTollgate = ({visible, setVisible}: {visible: boolean, setVisible: Rea
         <Row className="detailed-h list-tollgate" gutter={[16, 16]} style={{backgroundColor: 'white', paddingTop:'0px'}}>
           <Col xs={{ span: 12 }} lg={{ span: 24}}>
             <Row style={{height: '357px', overflowY: 'auto'}} className="row-modal-list-view tollgate-body">
-              <Col xs={{ span: 12 }} lg={{ span: 11}}>
+              <Col xs={{ span: 12 }} lg={{ span: 8}} style={{paddingRight:'10px'}}>
                 <p style={{marginBottom:'25px'}}>Draft</p>
                 <p style={{marginBottom:'25px'}}>Work Request (WR)</p>
                 <p style={{marginBottom:'25px'}}>Work Plan (WP)</p>
@@ -70,7 +89,7 @@ const ModalTollgate = ({visible, setVisible}: {visible: boolean, setVisible: Rea
                 <p style={{marginBottom:'25px'}}>Substantial Completion</p>
                 <p style={{marginBottom:'25px'}}>Closed</p>
               </Col>
-              <Col xs={{ span: 12 }} lg={{ span: 13}}>
+              <Col xs={{ span: 12 }} lg={{ span: 8}}>
                 <p className='calendar-toollgate'>
                   <RangePicker bordered={false} format={dateFormatList}/>
                 </p>
@@ -110,6 +129,151 @@ const ModalTollgate = ({visible, setVisible}: {visible: boolean, setVisible: Rea
                 <p className='calendar-toollgate'>
                   <RangePicker bordered={false} format={dateFormatList}/>
                 </p>
+              </Col>
+              <Col xs={{ span: 12 }} lg={{ span: 8}} style={{paddingLeft:'10px'}}>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>0</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>0</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>1</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>1</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>2</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>2</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>3</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>3</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>4</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>4</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>5</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>5</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>6</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>6</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>7</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>7</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>8</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>8</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>9</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>9</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>10</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>10</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>11</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>11</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-l'>12</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 2}}>
+                    <p className='duration-toollgate border-tollgate' >|</p>
+                  </Col>
+                  <Col xs={{ span: 12 }} lg={{ span: 11}}>
+                    <p className='duration-toollgate duration-toollgate-r'>12</p>
+                  </Col>
+                </Row>
               </Col>
             </Row>
             <div style={{textAlign:'end', marginTop:'10px'}} >
