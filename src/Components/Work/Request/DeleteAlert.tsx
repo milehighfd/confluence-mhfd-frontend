9baa39e3e6c9d39ba6b1007f7af834d989a0a721
JORGE <egrojmonroy@gmail.com>
2022-12-16 10:05:31 -0400

    chagne text in delete

--- src/Components/Work/Request/DeleteAlert.tsx
+++ src/Components/Work/Request/DeleteAlert.tsx
@@ -27,7 +27,7 @@ export const DeleteAlert = ({ visibleAlert, setVisibleAlert, action, name }: {
     >
       <Row className="detailed-h" gutter={[16, 8]}>
           <Col xs={{ span: 12 }} lg={{ span: 13 }}>
-            <h1 style={{marginTop: '15px'}}>Confirm Project Copy
+            <h1 style={{marginTop: '15px'}}>Confirm Project Deletion
             </h1>
           </Col>
           <Col xs={{ span: 12 }} lg={{ span: 11 }} style={{textAlign: 'end'}}>
@@ -36,7 +36,7 @@ export const DeleteAlert = ({ visibleAlert, setVisibleAlert, action, name }: {
         </Row>
         <Row className="detailed-h" gutter={[16, 8]} style={{backgroundColor: 'white'}}>
           <Col xs={{ span: 48 }} lg={{ span: 24 }} style={{color: '#11093c'}}>
-            <p style={{color: '#11093c', fontWeight: '500', paddingBottom: '10px'}}>Please confirm that the project will be copied to current year boarddeleted.</p>
+            <p style={{color: '#11093c', fontWeight: '500', paddingBottom: '10px'}}>Please confirm that the project will be deleted. This includes all cards of the same project located across various years.</p>
           </Col>
           <Col xs={{ span: 24 }} lg={{ span: 12 }} style={{color: '#11093c'}}>
                 {/* <h2>Saving will create a draft project within {sponsor}'s Work Request. Do you want to continue?</h2> */}
