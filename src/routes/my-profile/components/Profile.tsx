4891165f686cfede0d35914f8f757e6f3ac307c6
Donwilo <burgoadanilson@gmail.com>
2023-02-06 14:04:19 -0400

    Add first name, last name , populate organization

--- src/routes/my-profile/components/Profile.tsx
+++ src/routes/my-profile/components/Profile.tsx
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from "react";
 import { useProfileState } from "hook/profileHook";
 import * as datasets from "../../../Config/datasets";
 import { SERVER } from "../../../Config/Server.config";
+import { useAppUserDispatch } from "hook/useAppUser";
+import { getGroupList } from "routes/portfolio-view/components/ListUtils";
 
-
+const STATUS = 'status', JURISDICTION = 'jurisdiction',
+COUNTY = 'county', SERVICE_AREA = 'servicearea', CONSULTANT = 'consultant',
+CONTRACTOR = 'contractor', STREAMS = 'streams';
 const { Option } = Select;
 const Profile = () => {
   const [editProfile, setEditProfile] = useState(false);
@@ -17,6 +21,8 @@ const Profile = () => {
   const [jurisdictionList, setJurisdictionList] = useState<any[]>([]);
   const [serviceAreaList, setServiceAreaList] = useState<any[]>([]);
   const [email, setEmail] = useState(user.email);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
   const [phone, setPhone] = useState(user.phone);
   const [organization,setOrganization] = useState(user.organization);
   const [city,setCity] = useState(user.city);
@@ -24,7 +30,17 @@ const Profile = () => {
   const [serviceArea,setServiceArea] = useState(user.serviceArea);
   const [zoomarea,setZoomArea] = useState(user.zoomarea);
   const [disable,setDisable] = useState(false);
-  const [user_id,setUser_id] = useState(user.user_id);
+  const [save,setsave] = useState(false);
+  const {
+    replaceAppUser,
+    saveUserInformation,   
+  } = useAppUserDispatch();
+  const [consultantList, setConsultantList] = useState([]);
+  const [contractorList, setContractorList] = useState([]);
+  const [optionCounty, setOptionCounty] = useState({})
+  const [optionJurisdiction, setOptionJurisdiction] = useState({})
+  const [optionConsultant, setOptionConsultant] = useState({})
+  const [optionContractor, setOptionContractor] = useState({})
 
   useEffect(() => {
     setDataAutocomplete(groupOrganization.map((item: any) => {
@@ -32,52 +48,63 @@ const Profile = () => {
     }));
   }, [groupOrganization]);
 
-  useEffect(() => {
-    console.log(email)
-  }, [email]);
-
-  useEffect(() => {
-    console.log(user_id+2);
+  
+  useEffect(() => {   
     if (editProfile) {
       setDisable(false);
     } else {
-      setDisable(true);
-      console.log("Upload data");      
-      datasets.putData(SERVER.USER_UPDATE, {
-        email,
-        phone,    
-        organization,
-        city,
-        county,
-        serviceArea,
-        zoomarea    
-      }, datasets.getToken()).then((data) => {
-        console.log(data);
-      })
-        .catch((e) => {
-          console.log(e);
-        });
+      setDisable(true);      
     }
   }, [editProfile]);
+
+
   
   useEffect(() => {
     datasets.getData(`${SERVER.ALL_GROUP_ORGANIZATION}`)
       .then((rows) => {
-        console.log(rows.county);
         setCountyList(rows.county.map((item: any) => {
           return { key: item.state_county_id , value : item.county_name, label : item.county_name }
-        }));
+        }).filter((data:any)=>!!data.value));
         setJurisdictionList(rows.jurisdiction.map((item: any) => {
           return { key: item.code_local_government_id , value: item.local_government_name, label : item.local_government_name }
-        }));
+        }).filter((data:any)=>!!data.value));
         setServiceAreaList(rows.servicearea.map((item: any) => {
           return { key: item.code_service_area_id, value: item.service_area_name, label : item.service_area_name }
-        }));
+        }).filter((data:any)=>!!data.value));
+        getGroupList(CONSULTANT).then((valuesGroups) => {
+          const groups = valuesGroups.groups;
+          setConsultantList(groups.map((item: any) => {
+            return { key: item.id, value: item.name, label : item.name }
+          }).filter((data:any)=>!!data.value));
+        });
+        getGroupList(CONTRACTOR).then((valuesGroups) => {
+          const groups = valuesGroups.groups; 
+          setContractorList(groups.map((item: any) => { 
+            return { key: item.id, value: item.name, label : item.name } 
+          }).filter((data:any)=>!!data.value));
+        });        
+        
       })
       .catch((e) => {
         console.log(e);
-      })   
+      })             
   }, []);
+
+  useEffect(() => {   
+    let userTestStatus: { label:string, options: Array<{key: number, value: string , label : string}> }[] = [
+      {label:"Jurisdiction",options:jurisdictionList}      
+    ];
+    const jurisdiction = {label : "Jurisdiction",options:jurisdictionList}
+    const county = {label : "County" , options:countyList }
+    const consultant = {label:"Consultant",options:consultantList}
+    const contractor = {label:"Contractor",options:contractorList}
+    setOptionCounty(county);
+    setOptionJurisdiction(jurisdiction);
+    setOptionConsultant(consultant);
+    setOptionContractor(contractor);
+    console.log(userTestStatus)
+    console.log(contractor)
+  }, [jurisdictionList,countyList,consultantList,contractorList]);
 
   function isNull(text: string) {
     if(!text){
@@ -85,8 +112,40 @@ const Profile = () => {
     }else{
       return (text)
     }
-  }
-  
+  } 
+
+  const handleClick = () => {
+    setEditProfile(!editProfile)
+    if (!save) {
+      setsave(!save)
+      console.log("ENTER EDIT")
+    } else {
+      console.log("Upload data");
+      datasets.putData(SERVER.USER_UPDATE, {
+        email,
+        phone,
+        organization,
+        city,
+        county,
+        serviceArea,
+        zoomarea,
+        firstName,
+        lastName
+      }, datasets.getToken()).then((data) => {
+        console.log(data);
+      }).then(() => {
+        setsave(!save)
+        console.log("EXIT EDIT")
+        datasets.getData(SERVER.ME, datasets.getToken()).then(async result => {
+          replaceAppUser(result);
+          saveUserInformation(result)
+        });
+      })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+  };
 
   return (
     <div className="profile-myprofile">
@@ -112,6 +171,24 @@ const Profile = () => {
       <div style={{textAlign:'initial', padding:'10px', marginTop:'10px'}} className="profile">
         <Row >
           <Col xs={{ span: 24}} lg={{ span: 9 }}>
+            <p className="color-sub" style={{paddingBottom:'10px' }}>First Name</p>
+          </Col>
+          <Col xs={{ span: 24}} lg={{ span: 15 }}>
+            {editProfile ?
+              <input onChange={(e) => setFirstName(e.target.value)} className="input-profile" type="text" placeholder={isNull(firstName)} style={{border:'1px solid #d9d9d9', borderRadius:'15px', padding:'3px 8px', width:'100%',marginBottom:'15px'}}></input>
+              :<p style={{paddingBottom:'10px' }}>{isNull(firstName)}</p>
+            }
+          </Col>
+          <Col xs={{ span: 24}} lg={{ span: 9 }}>
+            <p className="color-sub" style={{paddingBottom:'10px' }}>Last Name</p>
+          </Col>
+          <Col xs={{ span: 24}} lg={{ span: 15 }}>
+            {editProfile ?
+              <input onChange={(e) => setLastName(e.target.value)} className="input-profile" type="text" placeholder={isNull(lastName)} style={{border:'1px solid #d9d9d9', borderRadius:'15px', padding:'3px 8px', width:'100%',marginBottom:'15px'}}></input>
+              :<p style={{paddingBottom:'10px' }}>{isNull(lastName)}</p>
+            }
+          </Col>
+          <Col xs={{ span: 24}} lg={{ span: 9 }}>
             <p className="color-sub" style={{paddingBottom:'10px' }}>Email</p>
           </Col>
           <Col xs={{ span: 24}} lg={{ span: 15 }}>
@@ -133,7 +210,7 @@ const Profile = () => {
             <p className="color-sub" style={{paddingBottom:'10px' }}>Organization</p>
           </Col>
           <Col xs={{ span: 24 }} lg={{ span: 15 }}>           
-              <Select onChange={(value) => setOrganization(value)} disabled={disable} defaultValue="MHFD District Boundary" style={{ width: '100%', marginBottom: '20px' }} getPopupContainer={(trigger: any) => trigger.parentNode}>
+              <Select  onChange={(value) => setOrganization(value)} disabled={disable} options={[optionCounty,optionJurisdiction,optionConsultant,optionContractor]} defaultValue="MHFD District Boundary" style={{ width: '100%', marginBottom: '20px' }} getPopupContainer={(trigger: any) => trigger.parentNode}>
                 <Option value="MHFD District Boundary">{isNull(organization)}</Option>
               </Select>              
           </Col>
@@ -174,7 +251,7 @@ const Profile = () => {
           </Col>
         </Row>
         <div className="foot-profile">
-          <Button style={{width:'100%'}} className="btn-purple" onClick={()=>{setEditProfile(!editProfile)}}>{editProfile ? 'Save':'Edit Profile'}</Button>
+          <Button style={{width:'100%'}} className="btn-purple" onClick={handleClick}>{editProfile ? 'Save':'Edit Profile'}</Button>
         </div>
       </div>
     </div>
