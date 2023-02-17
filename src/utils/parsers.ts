053d835419cac5d3aae125b0f5dfd84f74780c8b
Lua <xdaddisxd@gmail.com>
2023-02-17 18:02:24 -0400

    add functions to parse servicearea,county,sponsor

---
+++ src/utils/parsers.ts
@@ -0,0 +1,44 @@
+import { SPONSOR_ID } from '../constants/databaseConstants';
+
+export const getSponsors = (projectPartners: any) => {
+  const sponsors = projectPartners.reduce((accumulator: string, current: any) => {
+    const sa = current?.business_associate?.business_name || '';
+    let value = accumulator;
+    if (sa && current.code_partner_type_id === SPONSOR_ID) {
+      if (value) {
+        value += ',';
+      }
+      value += sa;
+    }  
+    return value;
+  }, '');
+  return sponsors;
+};
+
+export const getServiceAreas = (serviceAreas: any) => {
+  return serviceAreas.reduce((accumulator: string, pl: any) => {
+    const sa = pl?.CODE_SERVICE_AREA?.service_area_name || '';
+    let value = accumulator;
+    if (sa) {
+      if (value) {
+        value += ',';
+      }
+      value += (sa + ' Service Area');
+    } 
+    return value;
+  }, '');
+};
+
+export const getCounties = (projectsCounties: any) => {
+  return projectsCounties?.reduce((accumulator: string, pl: any) => {
+    const county = pl?.CODE_STATE_COUNTY?.county_name || '';
+    let value = accumulator;
+    if (county) {
+      if (value) {
+        value += ',';
+      }
+      value += (county + ' County');
+    } 
+    return value;
+  }, '');
+};
