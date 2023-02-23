894a10659cca86fff5798cd8b3f2cb9ebe8bf3b6
Donwilo <burgoadanilson@gmail.com>
2023-02-23 16:43:36 -0400

    Add team module

--- src/Components/Shared/Modals/TeamModal.tsx
+++ src/Components/Shared/Modals/TeamModal.tsx
@@ -1,179 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col } from 'antd';
 import { DownOutlined, SearchOutlined, SendOutlined, UpOutlined } from '@ant-design/icons';
+import { getTeam } from '../../../utils/parsers';
+import { useDetailedState } from "hook/detailedHook";
 
 const TeamModal = () => {
   const [openChat, setOpenChat] = useState(true);
+  const [data, setData] = useState<any>([]);
+  const { detailed } = useDetailedState();
+  useEffect(() => {
+    setData(getTeam(detailed?.project_staffs || []));    
+  }, [detailed]);
   return <>
-    <Row>
-      <Col span={4}>
-        <img src="/picture/user.png" alt="" height="35px" />
-      </Col>
-      <Col span={13}>
-        <h6 style={{fontWeight: 500}}>Finnegan Cooke</h6>
-        <p>Watershed Manager</p>
-      </Col>
-      <Col span={6} style={{ textAlign: 'right' }}>
-        <span>MHFD</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col span={4}>
-        <img src="/picture/user01.png" alt="" height="35px" />
-      </Col>
-      <Col span={13}>
-        <h6 style={{fontWeight: 500}}>Noel Holland</h6>
-        <p>SEMSWA</p>
-      </Col>
-      <Col span={6} style={{ textAlign: 'right' }}>
-        <span>Arvada</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col span={4}>
-        <img src="/Icons/icon-28.svg" alt="" height="35px" style={{borderRadius: '50%'}}/>
-      </Col>
-      <Col span={13}>
-        <h6 style={{fontWeight: 500}}>Conner Gilmore</h6>
-        <p>Project Manager</p>
-      </Col>
-      <Col span={6} style={{ textAlign: 'right' }}>
-        <span>Applegate</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col span={4}>
-        <img src="/Icons/icon-28.svg" alt="" height="35px" style={{borderRadius: '50%'}}/>
-      </Col>
-      <Col span={13}>
-        <h6 style={{fontWeight: 500}}>Megan Leonard </h6>
-        <p>Analyst</p>
-      </Col>
-      <Col span={6} style={{ textAlign: 'right' }}>
-        <span>Alpha</span>
-      </Col>
-    </Row>
-    <Row>
-      <Col span={4}>
-        <img src="/Icons/icon-28.svg" alt="" height="35px" style={{borderRadius: '50%'}}/>
-      </Col>
-      <Col span={13}>
-        <h6 style={{fontWeight: 500}}>Serena Keller</h6>
-        <p>Project Engineer</p>
-      </Col>
-      <Col span={6} style={{ textAlign: 'right' }}>
-        <span>MHFD</span>
-      </Col>
-    </Row>
-    <Row style={{paddingBottom: '15px'}}>
-      <Col span={14}>
-        <h5 style={{textDecoration: 'none'}}>
-          Comments
-        </h5>
-      </Col>
-      <Col span={10} style={{textAlign:'end'}}>
-        <SearchOutlined style={{paddingRight:'10px'}}/>
-        {openChat && <UpOutlined onClick={() => (setOpenChat(false))}/>}
-        {!openChat && <DownOutlined onClick={() => (setOpenChat(true))}/>}
-      </Col>
-      </Row>
-    <div className="chat-00" style={!openChat ? {height: '90px'} : {}}>
-      <div className='chat-body-text'  style={!openChat ? {height: '0px'} : {}}>
-        <Row>
-          <Col span={3}>
-            <img src="/picture/user.png" alt="" height="20px" />
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Finnegan Cooke<span>5h ago</span></h6>
-            <p>How long until 30% plans are ready for review?</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/Icons/icon-28.svg" alt="" height="20px"  style={{borderRadius: '50%'}}/>
-          </Col>
-          <Col span={21}>
-            <h6  style={{fontWeight: 500}}>Noel Holland<span>4h ago</span></h6>
-            <p>Probably April 1</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/picture/user.png" alt="" height="20px" />
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Finnegan Cooke<span>3h ago</span></h6>
-            <p>Thanks, we'll have those turned around in a couple of weeks.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/picture/user01.png" alt="" height="20px" />
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Megan Leonard<span>2h ago</span></h6>
-            <p>It might take SEMSWA closer to three weeks.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/Icons/icon-28.svg" alt="" height="20px" style={{borderRadius: '50%'}}/>
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Serena Keller<span>1h ago</span></h6>
-            <p>We don't have any concerns at this point.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/Icons/icon-28.svg" alt="" height="20px" style={{borderRadius: '50%'}}/>
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Serena Keller<span>Just now</span></h6>
-            <p>The boulder drop and culvert crossing look good in the renderings, we did something similar on Niver Trib.</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/Icons/icon-28.svg" alt="" height="20px" style={{borderRadius: '50%'}}/>
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Finnegan Cooke<span>5h ago</span></h6>
-            <p>How long until 30% plans are ready for review?</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={3}>
-            <img src="/Icons/icon-28.svg" alt="" height="20px" style={{borderRadius: '50%'}}/>
-          </Col>
-          <Col span={21}>
-            <h6 style={{fontWeight: 500}}>Finnegan Cooke<span>5h ago</span></h6>
-            <p>How long until 30% plans are ready for review?</p>
-          </Col>
-        </Row>
-      </div>
-      <Row style={openChat ? {paddingTop: '15px'} : {}}>
-        <Col span={3}>
-          <img src="/picture/user.png" alt="" height="20px" />
+    {data.map((item: any)=>(
+      <Row key={item.key}>
+        <Col span={4}>
+          <img src="/picture/user.png" alt="" height="35px" />
         </Col>
-        <Col span={21}>
-          <textarea className='chat-text'></textarea>
+        <Col span={13}>
+          <h6 style={{ fontWeight:500 }}>{item.fullName}</h6>
+          <p>{item.roleType}</p>
+        </Col>
+        <Col span={6} style={{ textAlign: 'right' }}>
+          <span>{item.organization}</span>
         </Col>
       </Row>
-    </div>
-    <div style={{textAlign: 'end'}}>
-      <button className='chat-button'>
-        <SendOutlined
-          style={{
-            marginBottom: '0px',
-            color: 'white',
-            transform: 'rotate(-45deg)',
-            marginRight:'5px'
-          }}
-        />
-        Send
-      </button>
-    </div>
+    ))}
   </>
 };
 
