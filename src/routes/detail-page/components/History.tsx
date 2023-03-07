82c95a57937503bcfaa50cc3753963e61d190b59
dottyy <dottykurmi123@gmail.com>
2023-03-07 10:57:58 -0400

    edit scroll

--- src/routes/detail-page/components/History.tsx
+++ src/routes/detail-page/components/History.tsx
@@ -18,7 +18,7 @@ const History = () => {
           <div className="line-01" style={{marginBottom:'15px', marginTop:'20px'}}></div>
         </Col>
       </Row>
-      <Row style={{marginBottom:'70px'}}>
+      <Row style={{marginBottom:'150px'}}>
         <Col xs={{ span: 24 }} lg={{ span: 24 }} className='history-body'>
         <ul className="list-history">
           {
