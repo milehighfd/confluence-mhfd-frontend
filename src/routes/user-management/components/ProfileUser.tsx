29922919dfb6cc4bc3a5d14d875b7a004378b0f3
dottyy <dottykurmi123@gmail.com>
2023-03-09 10:45:35 -0400

    edit style

--- src/routes/user-management/components/ProfileUser.tsx
+++ src/routes/user-management/components/ProfileUser.tsx
@@ -363,7 +363,7 @@ const ProfileUser = ({ record, saveUser, deleteUser, type, deleteUserDatabase }:
                   //console.log('indexx', index);
                   if (index < 3) {
                   }
-                  return <RadioDesignation key={index} index={index} value={item.value === 'MHFD Senior Manager'? 'MHFD Admin': item.value} name={item.value === 'MHFD Senior Manager'? 'MHFD Admin': item.value}/>;
+                  return <RadioDesignation key={index} index={index} value={item.value} name={item.value}/>;
                 })}
               </Col>
             </Radio.Group>
