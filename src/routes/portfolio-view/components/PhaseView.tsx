d9706210bc78fa6908f970dcb015e371a4908ae0
dottyy <dottykurmi123@gmail.com>
2023-03-15 09:48:59 -0400

    edit scroll

--- src/routes/portfolio-view/components/PhaseView.tsx
+++ src/routes/portfolio-view/components/PhaseView.tsx
@@ -682,12 +682,10 @@ const PhaseView = (
             onScroll={(e: any) => {
               let dr: any = phaseRef.current;
               if (searchRef.current[indexParent] && headerRef.current) {
-                console.log(headerRef.current?.scrollLeft, headerRef.current?.scrollTop, dr.scrollLeft, dr.scrollTop, 'INI')
+                // console.log(headerRef.current?.scrollLeft, headerRef.current?.scrollTop, dr.scrollLeft, dr.scrollTop, 'INI',e.target.scrollTop,e.target.scrollLeft)
                 searchRef.current[indexParent].scrollTo(dr.scrollLeft, dr.scrollTop);
-                headerRef.current?.scrollTo(dr.scrollLeft, 0)
-                console.log(headerRef.current?.scrollLeft, headerRef.current?.scrollTop, dr.scrollLeft, dr.scrollTop, "FIN")
-                // dr.scrollTo(headerRef.current?.scrollLeft , dr.scrollTop)
-               
+                headerRef.current?.scrollTo(dr.scrollLeft, dr.scrollTop);
+                // console.log(headerRef.current?.scrollLeft, headerRef.current?.scrollTop, dr.scrollLeft, dr.scrollTop, "FIN")
               }
             }}            
           >
