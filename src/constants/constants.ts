06edb11ca39744533c6f217288442f951d9a8c5c
JORGE <egrojmonroy@gmail.com>
2023-03-28 15:52:08 -0400

    use ENV for all project tables

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -365,7 +365,7 @@ export const ROUTINE_MAINTENANCE = {
 export const PROJECTS_POLYGONS = 'projects_polygon_';
 // export const MHFD_PROJECTS = 'mhfd_projects'+ (process.env.REACT_APP_NODE_ENV ? ( '_' + ((process.env.REACT_APP_NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'prod' ) ? 'prod' :  process.env.REACT_APP_NODE_ENV) ) : '_dev');
 export const MHFD_PROJECTS = 'mhfd_projects'+ (process.env.REACT_APP_NODE_ENV ? ( '_' + ((process.env.REACT_APP_NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'prod' ) ? 'prod' :  process.env.REACT_APP_NODE_ENV) ) : '_dev');
-export const PROJECTS_DRAFT = 'mhfd_projects_test';
+export const PROJECTS_DRAFT = 'mhfd_projects'+ (process.env.REACT_APP_NODE_ENV ? ( '_' + ((process.env.REACT_APP_NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'prod' ) ? 'prod' :  process.env.REACT_APP_NODE_ENV) ) : '_dev');
 
 // export const MHFD_PROJECTS = 'mhfd_projects' + (process.env.REACT_APP_NODE_ENV ? ( '_' + ((process.env.REACT_APP_NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'prod' ) ? 'prod' :  process.env.REACT_APP_NODE_ENV) ) : '_dev')
 // // export const PROJECTS_DRAFT = 'mhfd_projects_created' + (process.env.REACT_APP_NODE_ENV ? ( '_' + ((process.env.REACT_APP_NODE_ENV === 'production' || process.env.REACT_APP_NODE_ENV === 'prod' ) ? 'prod' :  process.env.REACT_APP_NODE_ENV) ) : '_dev')
