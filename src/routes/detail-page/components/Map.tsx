ca501716693ee210bfee52b90150dddfae208071
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-04-02 22:30:03 -0400

    add map reference

--- src/routes/detail-page/components/Map.tsx
+++ src/routes/detail-page/components/Map.tsx
@@ -604,12 +604,12 @@ useEffect(() => {
           <h3 style={{marginBottom:'15px', marginTop:'20px'}} id="maps">MAP</h3>
         </Col>
         <Col xs={{ span: 24 }} lg={{ span: 20 }} style={{alignSelf: 'center'}}>
-          <div className="line-01"></div>
+          <div className="line-01 mapboxgl-canvas"></div>
         </Col>
       </Row>
       <Row>
         <Col xs={{ span: 24 }} lg={{ span: 24 }} className="table-financials-modal">
-          <div id="map2" style={{height:'520px', width:'100%', borderRadius:'1%'}} className="mapboxgl-canvas"></div>
+          <div id="map2" style={{height:'520px', width:'100%', borderRadius:'1%'}} ></div>
           {/* <img src="/picture/map.png" width='100%'/> */}
         </Col>
       </Row>
