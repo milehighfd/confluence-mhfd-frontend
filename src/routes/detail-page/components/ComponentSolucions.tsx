b56ecea0f251573a73627d522d674817049a59d2
angel cussi <angel.cussi@vizonomy@vizonomy.com>
2023-04-14 11:05:47 -0400

    total proposed

--- src/routes/detail-page/components/ComponentSolucions.tsx
+++ src/routes/detail-page/components/ComponentSolucions.tsx
@@ -73,7 +73,7 @@ const ComponentSolucions = () => {
         <Col xs={{ span: 24 }} lg={{ span: 24 }} className="table-detail-modal">
           <Table dataSource={componentsOfProblems ?  dataSolution : {}} columns={columns} pagination={false}/>
           {componentsOfProblems.length > 0 && <div className="value-total">
-            <p className="table-total" style={{width:'calc(30% + 0px)'}}>{`Total Estimated Cost (${totalComponents})`}</p><p style={{width:'calc(20% + 0px)'}}>${new Intl.NumberFormat("en-EN").format(total)}</p>
+            <p className="table-total" style={{width:'calc(30% + 0px)'}}>{`Total Proposed Cost (${totalComponents})`}</p><p style={{width:'calc(20% + 0px)'}}>${new Intl.NumberFormat("en-EN").format(total)}</p>
           </div>}
         </Col>
       </Row>
