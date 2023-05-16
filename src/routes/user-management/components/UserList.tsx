3526d4a110ded37d54625f64f1e78ac7a7d6ff5e
dottyy <dottykurmi123@gmail.com>
2023-05-16 10:47:45 -0400

    add load un usser management

--- src/routes/user-management/components/UserList.tsx
+++ src/routes/user-management/components/UserList.tsx
@@ -14,6 +14,7 @@ import { useUsersDispatch, useUsersState } from "hook/usersHook";
 import { PAGE_USER } from "constants/constants";
 import UserFilters from "Components/User/UserFilters";
 import UserMngFilters from "./UserMngFilters";
+import LoadingViewOverall from "Components/Loading-overall/LoadingViewOverall";
 
 const { TabPane } = Tabs;
 const tabKeys = ['Roles Management', 'Users Management', 'Project Management'];
@@ -28,27 +29,6 @@ const titleCase = (str:any)=> {
   return splitStr.join(' '); 
 }
 
-const getUser = (saveUser: Function, setUser: Function, url: string, setTotal: Function) => {
-  datasets.getData(url, datasets.getToken()).then(res => {
-    const arrayUsers = res.users.map((elem: any) => {
-      return {
-        ...elem,
-        name: [elem.name, elem.email, elem.photo],
-        statusAccount: elem.activated === true? 'Active': 'Inactive',
-        serviceArea: elem.serviceArea !== null ? elem.serviceArea : '-',
-        city: elem.city !== null ? elem.city : '-',
-        county: elem.county !== null ? elem.county : '-',
-        key:elem.user_id,
-        //designation: titleCase(elem.designation)
-      }
-    });
-    if (res.users) {
-      saveUser(res.users);
-      setUser(arrayUsers);
-      setTotal(res.totalPages * 10);
-    }
-  });
-}
 
 const UserList = () => {
   interface DataType {
@@ -156,6 +136,7 @@ const UserList = () => {
   const [userDeleted, setUserDeleted] = useState<Array<User>>([]);
   const [userActivatedState, setUserActivatedState] = useState<Array<User>>([]);
   const [totalUsersActivated, setTotalUsersActivated] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const [messageError, setMessageError] = useState({ message: '', color: '#28C499' });
 
@@ -169,7 +150,28 @@ const UserList = () => {
   const [userSelected, setUserSelected] = useState<any>();
   let displayedTabKey = tabKeys;
   const [optionSelect, setOptionSelect] = useState('Approved Users')
-  let items = [
+  const getUser = (saveUser: Function, setUser: Function, url: string, setTotal: Function) => {
+    datasets.getData(url, datasets.getToken()).then(res => {
+      const arrayUsers = res.users.map((elem: any) => {
+        setIsLoading(false)
+        return {
+          ...elem,
+          name: [elem.name, elem.email, elem.photo],
+          statusAccount: elem.activated === true? 'Active': 'Inactive',
+          serviceArea: elem.serviceArea !== null ? elem.serviceArea : '-',
+          city: elem.city !== null ? elem.city : '-',
+          county: elem.county !== null ? elem.county : '-',
+          key:elem.user_id,
+        }
+      });
+      if (res.users) {
+        saveUser(res.users);
+        setUser(arrayUsers);
+        setTotal(res.totalPages * 10);
+      }
+    });
+  }
+let items = [
     { key: 'edit-user', label: 'Edit User' },
     //{ key: 'message-user', label: 'Message User' },
     { key: 'delete-user', label: 'Delete User' },
@@ -356,10 +358,11 @@ const UserList = () => {
 
   // console.log(optionSelect)
   return <>
+  {isLoading && <LoadingViewOverall />}
     <div>
       <div className="head-list">
         <div className="list-view-head" >
-        <Select className="select-type" placeholder="Approved Users" placement="bottomLeft" style={{marginTop: '5px', marginLeft:'2px'}} value={optionSelect?? optionSelect} onChange={(e)=>{setOptionSelect(e)}}>
+        <Select className="select-type" placeholder="Approved Users" placement="bottomLeft" style={{marginTop: '5px', marginLeft:'2px'}} value={optionSelect?? optionSelect} onChange={(e)=>{setOptionSelect(e); setIsLoading(true)}}>
             <Option value="Approved Users"><span style={{paddingLeft:'10px'}}>Approved Users</span></Option>
             <Option value="Pending User Requests"><span style={{paddingLeft:'3px'}}>Pending User Requests</span></Option>
             <Option value="Deleted Users"><span style={{paddingLeft:'10px'}}>Deleted Users</span></Option>
