78db4f997ba14bc9d2a38e0f8de14273ff686ce1
Isra <greix006@gmail.com>
2023-06-02 16:02:46 -0400

    Add new component PreSignUp

---
+++ src/routes/sign-up/components/PreSignUp.tsx
@@ -0,0 +1,33 @@
+import { Button } from "antd";
+import React, { useState } from "react";
+
+
+
+const PreSignUp = ()=>{
+const [emailOnBlur, setEmailOnBlur] = useState(false);
+  
+return    <div>
+       <span style={{fontSize:'30px', color:'#251863', paddingBottom:'20px'}}>Welcome to MHFD's Confluence</span><br/><br/> 
+       <span style={{fontSize:'22px', marginBottom:'10px', color:'#251863'}}>Let's begin the adventure</span><br/>
+       <div className="group login-form" style={{marginTop:'30px'}}> 
+          {
+            (emailOnBlur) && <label style={{color:'#88849D'}}> Email Address</label>
+          }
+          <input
+            placeholder="Email Address"
+            type="email"
+            name="email"            
+            onBlur={() => setEmailOnBlur(false)}
+            onClick={() => setEmailOnBlur(true)}
+            autoComplete="username"
+          />
+          <span className="highlight"></span>
+          <span className="bar"></span>
+        </div>
+       <Button className="btn-purple" block htmlType="submit">
+          Register by Email
+        </Button>
+    </div>
+}
+
+export default PreSignUp;
