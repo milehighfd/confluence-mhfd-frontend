034a1e1f39516b624bca4330916d2ad20627aac2
dottyy <dottykurmi123@gmail.com>
2023-06-06 10:01:52 -0400

    edit style

--- src/routes/sign-up/components/CheckYourEmailModal.tsx
+++ src/routes/sign-up/components/CheckYourEmailModal.tsx
@@ -7,22 +7,27 @@ type Props = {
 };
 const CheckYourEmailModal = ({ email }: Props) => {
   return <div className="main-conf">
-    <div className="group">
+    <div className="login-form group">
       <div className="letter-conf">
         <img src="Icons/letter.svg" alt="" />
       </div>
-      <div className="title-conf">
-        <span>Confirm Your Email Address</span><br /><br />
+      {/* <div className="title-conf"> */}
+        <h1>Confirm Your Email Address</h1>
+        {/* <br /><br /> */}
+      {/* </div> */}
+      <div className="resetText">
+        <p>Thank you for signing up. We have sent a confirmation email to <strong>{email}.</strong>  Check your email and click on the confirmation link to start using Confluence.</p>
+        <p>Haven't received your email after one minute? Please check your spam or junk folder. Click the button below to resend it.</p>
       </div>
-      <div className="all-text-conf">
+      {/* <div className="all-text-conf">
         <span className="text-conf" >Thank you for signing up. We have sent a confirmation email to <strong>{email}.</strong>  Check your email and click on the confirmation link to start using Confluence.</span><br /><br />
         <span style={{ fontSize: '18px', marginBottom: '10px', color: '#251863' }}>Haven't received your email after one minute? Please check your spam or junk folder. Click the button below to resend it.</span>
-      </div>
+      </div> */}
       <Button
         className="btn-purple"
         block
         htmlType="submit"
-        style={{ width: '400px', marginTop: '67px' }}
+        style={{ width: '400px', marginTop: '30px', height:'50px' }}
         onClick={() => onGenerateSignupLink(email, () => console.log('success'), () => console.log('error'))}
       >
         RESEND EMAIL
