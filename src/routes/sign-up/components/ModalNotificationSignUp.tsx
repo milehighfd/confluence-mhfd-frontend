4ed279789effc8221c93d7d6818b4d48a87a5a82
dottyy <dottykurmi123@gmail.com>
2023-06-06 15:17:38 -0400

    add modal in presingup

---
+++ src/routes/sign-up/components/ModalNotificationSignUp.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button, Col, Modal, Row} from 'antd';
+
+const ModalNotificationSignUp = ({ visible, setVisible, message }: {   visible: boolean, setVisible: any, message: string }) => {
+  const handleCancel = () => {
+    setVisible(false);
+  };
+  return (
+    <Modal
+      className="detailed-version detailed-upload-save"
+      style={{ top: 'calc(50vh - 100px)', width: '34%' }}
+      visible={visible}
+      onCancel={handleCancel}
+      forceRender={false}
+      destroyOnClose>
+      <div className="detailed">
+        <Row className="detailed-h" gutter={[16, 8]}>
+          <Col xs={{ span: 12 }} lg={{ span: 21 }}>
+            <h1 style={{marginTop: '15px'}}>{message}
+            </h1>
+          </Col>
+          <Col xs={{ span: 12 }} lg={{ span: 3 }} style={{textAlign: 'end'}}>
+            <Button className="btn-transparent" onClick={() => setVisible(false)}><img src="/Icons/icon-62.svg" alt="" height="15px" /></Button>
+          </Col>
+        </Row>
+        <Row className="detailed-h" gutter={[16, 8]} style={{backgroundColor: 'white'}}>
+            <Col xs={{ span: 24 }} lg={{ span: 24 }} style={{color: '#11093c', textAlign:'end'}}>
+              <button className="btn-purple"  style={{width: '95%', height:'auto', padding: '6px'}} onClick={()=>{setVisible(false)}}>CONTINUE</button>
+            </Col>
+        </Row>
+      </div>
+    </Modal>
+  )
+};
+
+export default ModalNotificationSignUp;
--- src/routes/sign-up/components/PreSingUpForm.tsx
+++ src/routes/sign-up/components/PreSingUpForm.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Row } from "antd";
 import { useSignup } from '../hooks/useSignup';
 import { onGenerateSignupLink } from '../utils';
+import ModalNotificationSignUp from './ModalNotificationSignUp';
 
 
 type PreSignUpFormProps = {
@@ -17,8 +18,11 @@ export const PreSignUpForm = ({ email, isValidEmail, sucessCallback, errorCallba
     setEmailOnBlur
   } = useSignup();
   
+  const [visible, setVisible] = useState(false);
 
   return (
+    <>
+    <ModalNotificationSignUp visible={visible} setVisible={setVisible} message='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
     <Form style={{ width: '420px' }} className="login-step01" id='login-form'>
       <h1>
         Welcome to MHFD's Confluence
@@ -32,7 +36,7 @@ export const PreSignUpForm = ({ email, isValidEmail, sucessCallback, errorCallba
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)} 
-          style={(email && email) ? { borderBottom: 'solid red 1px', paddingLeft: '10px' } : { paddingLeft: '10px' }} />
+          style={{ paddingLeft: '10px' }} />
         <span className="highlight"></span>
         <span className="bar"></span>
       </div>
@@ -46,5 +50,7 @@ export const PreSignUpForm = ({ email, isValidEmail, sucessCallback, errorCallba
         </Button>
       
     </Form>
+    </>
+    
   )
 }
