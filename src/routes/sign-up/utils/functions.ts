2ea8ca35e561bd80c40a3144fe43759f39eef68c
Lua <xdaddisxd@gmail.com>
2023-06-07 10:27:47 -0400

    add new endpoint to generate resetandconfirm email

--- src/routes/sign-up/utils/functions.ts
+++ src/routes/sign-up/utils/functions.ts
@@ -1,12 +1,20 @@
 import * as datasets from "../../../Config/datasets";
 import { SERVER } from '../../../Config/Server.config';
 
-export const onGenerateSignupLink = (email: string, cbSucess: Function, cbFall: Function) => {
-  datasets.postData(SERVER.GENERATE_SIGNUP_URL, { email })
+const makeCall = (url: string, data: any, cbSucess: Function, cbFall: Function) => {
+  datasets.postData(url, data)
   .then(() => {
     cbSucess();
   }).catch((err) => {
     console.log(err);
     cbFall();
   });
+};
+
+export const onGenerateSignupLink = (email: string, cbSucess: Function, cbFall: Function) => {
+  makeCall(SERVER.GENERATE_SIGNUP_URL, { email }, cbSucess, cbFall);
 }
+
+export const onGenerateResetAndConfirm = (email: string, cbSucess: Function, cbFall: Function) => {
+  makeCall(SERVER.GET_RESET_AND_CONFIRM, { email }, cbSucess, cbFall);
+};
