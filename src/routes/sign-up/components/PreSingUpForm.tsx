f71b5e5fda2db4b8f6da5d6a50625938c70c6f9b
dottyy <dottykurmi123@gmail.com>
2023-06-07 09:19:25 -0400

    edit text modal in pre-singup

--- src/routes/sign-up/components/PreSingUpForm.tsx
+++ src/routes/sign-up/components/PreSingUpForm.tsx
@@ -22,7 +22,7 @@ export const PreSignUpForm = ({ email, isValidEmail, sucessCallback, errorCallba
 
   return (
     <>
-    <ModalNotificationSignUp visible={visible} setVisible={setVisible} message='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
+    <ModalNotificationSignUp visible={visible} setVisible={setVisible} message='The email was unable to be sent. Please try again.'/>
     <Form style={{ width: '420px' }} className="login-step01" id='login-form'>
       <h1>
         Welcome to MHFD's Confluence
