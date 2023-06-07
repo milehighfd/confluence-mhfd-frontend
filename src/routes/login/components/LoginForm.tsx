1f9f82de4f0e807c1a80c7a06441cc5ed61e4a6b
Lua <xdaddisxd@gmail.com>
2023-06-07 10:28:25 -0400

    show email confirmation after login and invalid account

--- src/routes/login/components/LoginForm.tsx
+++ src/routes/login/components/LoginForm.tsx
@@ -11,6 +11,7 @@ import { useAppUserDispatch } from 'hook/useAppUser';
 import { useMapDispatch } from 'hook/mapHook';
 import { REQUIRED } from 'routes/login/components/constantsLogin';
 import { GlobalMapHook } from 'utils/globalMapHook';
+import CheckYourEmailModal from '../../sign-up/components/CheckYourEmailModal';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string()
@@ -32,6 +33,7 @@ const LoginView = () => {
     resetProfile,
     addNotifications
   } = useAppUserDispatch();
+  const [emailModal, setEmailModal] = useState(false);
   const { resetMap } = useMapDispatch();
   const [emailOnBlur, setEmailOnBlur] = useState(false);
   const [passwordOnBlur, setPasswordOnBlur] = useState(false);
@@ -73,6 +75,8 @@ const LoginView = () => {
             addNotifications(result);
           });
           setRedirect(true);
+        } else if (res?.order === 'need_reset_and_confirm'){
+          setEmailModal(true);
         } else {
           setMessage({
             message: 'Could not connect, check your email and password',
@@ -88,70 +92,78 @@ const LoginView = () => {
   }
 
   return (
-    <Form className="login-form" onFinish={handleSubmit}>
-      <img src="/Icons/Confluence-Color-Tagline.svg" alt="Confluence Logo" width="248px" />
-      <div className="group-container">
-        <div className="group">
-          {
-            (emailOnBlur || values.email.length > 0) && <label>Email Address</label>
-          }
-          <input
-            placeholder="Email Address"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={() => setEmailOnBlur(false)}
-            onClick={() => setEmailOnBlur(true)}
-            style={(errors.email && touched.email) ? { borderBottom: 'solid red 1px' } : { paddingLeft: '10px' }}
-            autoComplete="username"
-          />
-          <span className="highlight"></span>
-          <span className="bar"></span>
-        </div>
-        <div className="group">
-          {
-            (passwordOnBlur || values.password.length > 0) && <label>Enter Password</label>
-          }
-          <input
-            placeholder="Enter Password"
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={() => setPasswordOnBlur(false)}
-            onClick={() => setPasswordOnBlur(true)}
-            autoComplete="current-password"
-            style={(errors.password && touched.password) ? { borderBottom: 'solid red 1px' } : { paddingLeft: '10px' }}
-          />
-          <span className="highlight"></span>
-          <span className="bar"></span>
-        </div>
-        <div className="marbot-4">
-          <span>Don't have an account?</span>
-          <Link
-            to={'/pre-signup'}
-            className="login-form-forgot"
-          >
-            Sign-Up
-          </Link>
-          <Link
-            to={'/reset-password'}
-            className="login-forgot"
-          >
-            Forgot Password?
-          </Link>
-          <br/><br/>
-          <GoogleReCaptcha
-            onVerify={(token: string) => {
-              values.recaptcha = '' + (token !== 'null' ? token : '');
-            }}
-          />
-        </div>
-        <span style={{ color: message.color }}>&nbsp;&nbsp; {message.message}</span>
-        <Button className="btn-purple" block htmlType="submit">
-          Login
-        </Button>
-      </div>
-    </Form>
+    <>
+      {
+        !emailModal ? <Form className="login-form" onFinish={handleSubmit}>
+          <img src="/Icons/Confluence-Color-Tagline.svg" alt="Confluence Logo" width="248px" />
+          <div className="group-container">
+            <div className="group">
+              {
+                (emailOnBlur || values.email.length > 0) && <label>Email Address</label>
+              }
+              <input
+                placeholder="Email Address"
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={() => setEmailOnBlur(false)}
+                onClick={() => setEmailOnBlur(true)}
+                style={(errors.email && touched.email) ? { borderBottom: 'solid red 1px' } : { paddingLeft: '10px' }}
+                autoComplete="username"
+              />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <div className="group">
+              {
+                (passwordOnBlur || values.password.length > 0) && <label>Enter Password</label>
+              }
+              <input
+                placeholder="Enter Password"
+                type="password"
+                name="password"
+                onChange={handleChange}
+                onBlur={() => setPasswordOnBlur(false)}
+                onClick={() => setPasswordOnBlur(true)}
+                autoComplete="current-password"
+                style={(errors.password && touched.password) ? { borderBottom: 'solid red 1px' } : { paddingLeft: '10px' }}
+              />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <div className="marbot-4">
+              <span>Don't have an account?</span>
+              <Link
+                to={'/pre-signup'}
+                className="login-form-forgot"
+              >
+                Sign-Up
+              </Link>
+              <Link
+                to={'/reset-password'}
+                className="login-forgot"
+              >
+                Forgot Password?
+              </Link>
+              <br/><br/>
+              <GoogleReCaptcha
+                onVerify={(token: string) => {
+                  values.recaptcha = '' + (token !== 'null' ? token : '');
+                }}
+              />
+            </div>
+            <span style={{ color: message.color }}>&nbsp;&nbsp; {message.message}</span>
+            <Button className="btn-purple" block htmlType="submit">
+              Login
+            </Button>
+          </div>
+        </Form> :
+        <CheckYourEmailModal
+          email={values.email}
+          isResetPassword
+        />
+    }
+    </>
   );
 };
 
