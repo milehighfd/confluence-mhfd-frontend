31275b05740fd0d7c34d01d2889f65b6919a713d
Isra <greix006@gmail.com>
2023-06-07 12:40:53 -0400

    Modify cbError

--- src/routes/sign-up/components/CheckYourEmailModal.tsx
+++ src/routes/sign-up/components/CheckYourEmailModal.tsx
@@ -1,19 +1,21 @@
 import { Button } from "antd";
 import React from "react";
 import { onGenerateSignupLink, onGenerateResetAndConfirm } from '../utils';
+import ModalNotificationSignUp from "./ModalNotificationSignUp";
+import { set } from "react-ga";
 
 const confirmEmail = (email: string) => ({
   title: 'Confirm Your Email Address',
   text: <>Thank you for signing up. We have sent a confirmation email to <strong>{email}.</strong>  Check your email and click on the confirmation link to start using Confluence.</>,
   buttonText: 'RESEND EMAIL',
-  onClickButton: () => onGenerateSignupLink(email, () => console.log('success'), () => console.log('error'))
+  onClickButton: (email: string, cbError: Function) => onGenerateSignupLink(email, () => console.log('success'), cbError)
 });
 
 const resetPasswordEmail = (email: string) => ({
   title: 'Please reset your password',
   text: <>We have sent a password reset email to <strong>{email}.</strong> with next steps to reset your password.</>,
   buttonText: 'Send password reset email',
-  onClickButton: () => onGenerateResetAndConfirm(email, () => console.log('success'), () => console.log('error'))
+  onClickButton: (email: string, cbError: Function) => onGenerateResetAndConfirm(email, () => console.log('success'), cbError)
 });
 
 type Props = {
@@ -21,8 +23,10 @@ type Props = {
   isResetPassword?: boolean;
 };
 const CheckYourEmailModal = ({ email, isResetPassword }: Props) => {
-  const { title, text, buttonText } = isResetPassword ? resetPasswordEmail(email) : confirmEmail(email);
+  const [visible, setVisible] = React.useState(false);
+  const { title, text, buttonText, onClickButton } = isResetPassword ? resetPasswordEmail(email) : confirmEmail(email);
   return <div className="main-conf">
+     <ModalNotificationSignUp visible={visible} setVisible={setVisible} message='The email was unable to be sent. Please try again.'/>
     <div className="login-form" style={{textAlign: 'center', marginTop: '-20%'}}>
       <div className="letter-conf">
         <img src="Icons/letter.svg" alt="" />
@@ -38,7 +42,7 @@ const CheckYourEmailModal = ({ email, isResetPassword }: Props) => {
         className="btn-purple"
         block
         htmlType="submit"
-        onClick={() => onGenerateSignupLink(email, () => console.log('success'), () => console.log('error'))}
+        onClick={() => onClickButton(email, () => setVisible(true))}
       >
         {buttonText}
       </Button>
