9261694993e60bc84654d77efc46a9c0f6328692
dottyy <dottykurmi123@gmail.com>
2023-06-12 16:54:02 -0400

    add button back in pre signup

--- src/routes/sign-up/components/PreSignUpLayout.tsx
+++ src/routes/sign-up/components/PreSignUpLayout.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Layout, Row, Col } from 'antd';
 import CarouselAutoPlayView from '../../../Components/Shared/CarouselAutoPlay/CarouselAutoPlayView';
 import PreSignUp from './PreSignUp';
+import BackButton from 'routes/reset-password/components/BackButton';
 
 const PreSignUpLayout = () => {
   return (
     <Layout style={{ background: '#fff' }}>
       <Row>
         <Col xs={{ span: 24 }} lg={{ span: 11 }} className="login-hh">
-          <div className="login-step01" id="login-form">
+          <div className="login-step01" id="login-form" style={{top: '48%'}}>
+            <BackButton />
             <PreSignUp />
           </div>
         </Col>
