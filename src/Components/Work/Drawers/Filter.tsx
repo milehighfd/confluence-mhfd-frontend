275a0d393175ad7fa48e0868d7ccc40fbec33b0d
Jose Laura <pachonjcl@gmail.com>
2023-06-21 12:05:20 -0400

    fix: filters in WP

--- src/Components/Work/Drawers/Filter.tsx
+++ src/Components/Work/Drawers/Filter.tsx
@@ -16,11 +16,11 @@ const Filter = () => {
   const countiesFilterList: any[] = filterMap['project_counties'];
   const serviceAreasFilterList: any[] = filterMap['project_service_areas'];
   const priorityFilterList = useMemo(() => [
-    { label: '1', value: 1 },
-    { label: '2', value: 2 },
-    { label: '3', value: 3 },
-    { label: 'Over 3', value: 4 },
-    { label: 'Work Plan', value: 5 }
+    { label: '1', value: 0 },
+    { label: '2', value: 1 },
+    { label: '3', value: 2 },
+    { label: 'Over 3', value: 3 },
+    { label: 'Work Plan', value: 4 }
   ], []);
 
   const [jurisdictionSelected, setJurisdictionSelected] = useState<any[]>([]);
