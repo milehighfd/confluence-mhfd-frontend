5562af3ab11f7f20b4c27c98548b62996e42d322
Donwilo <burgoadanilson@gmail.com>
2023-07-27 16:14:47 -0400

    Add countywide component

--- src/Components/Project/TypeProjectComponents/ProjectGeometry.tsx
+++ src/Components/Project/TypeProjectComponents/ProjectGeometry.tsx
@@ -2,6 +2,7 @@ import { Radio, Select, Table } from 'antd';
 import React from 'react';
 import {  WINDOW_WIDTH } from 'constants/constants';
 import { DeleteOutlined } from '@ant-design/icons';
+import { Countywide } from './Countywide';
 
 interface Props {
   county: any;
@@ -9,10 +10,21 @@ interface Props {
   isDrawStateCapital: any;
   onClickDrawCapital: any;
   index: number;
+  showDraw: boolean;
+  setShowDraw: Function;
 }
 const { Option } = Select;
 
-export const ProjectGeometry = ({ county, setCounty, isDrawStateCapital, onClickDrawCapital, index }: Props) => {
+export const ProjectGeometry = ({ 
+  county, 
+  setCounty, 
+  isDrawStateCapital, 
+  onClickDrawCapital,
+  index,
+  showDraw,
+  setShowDraw
+}: Props) => {
+  
   //table geomeotry
   const dataSourceGeomeotry = [
     {
@@ -119,38 +131,17 @@ export const ProjectGeometry = ({ county, setCounty, isDrawStateCapital, onClick
     <>
       <div className="sub-title-project">
         <h5>{index}. PROJECT GEOMETRY *</h5>
-      </div>
-      <p className='text-default'>Projects are spatially defined by stream reaches.  Select the option below that best allows you to define the project.</p>
-      <div className='section-gemetry'>
-        <p>i. Is this a countywide project?</p>
-        <Radio.Group>
-          <Radio value="Yes"><span className='text-radio-btn'>Yes</span></Radio>
-          <Radio value="No"><span className='text-radio-btn'>No</span></Radio>
-        </Radio.Group>
-        <div className='section-county'>
-          <label className="sub-title">Select one or multiple counties </label>
-          <Select
-            mode="multiple"
-            placeholder={county?.length !== 0 ? county : "Select a County"}
-            style={{ width: '100%' }}
-            listHeight={WINDOW_WIDTH > 2554 ? (WINDOW_WIDTH > 3799 ? 500 : 320) : 256}
-            onChange={(county: any) => setCounty(county)}
-            value={county}>
-            <Option key='Adams' value='Adams'>Adams</Option>
-          </Select>
-        </div>
-        <p>ii. Is this project located on the South Platte River?</p>
-        <Radio.Group>
-          <Radio value="Yes"><span className='text-radio-btn'>Yes</span></Radio>
-          <Radio value="No"><span className='text-radio-btn'>No</span></Radio>
-        </Radio.Group>
-        <p className='sub-sub-title-projects'>iii. Draw your project geometry</p>
-      </div>
-
-      <div className={"draw " + (isDrawStateCapital ? 'active' : '')} onClick={onClickDrawCapital}>
+      </div>      
+      <Countywide
+        county={county}
+        setCounty={setCounty}
+        setShowDraw={setShowDraw}
+        showDraw={showDraw}
+      />
+      {showDraw && <div className={"draw " + (isDrawStateCapital ? 'active' : '')} onClick={onClickDrawCapital}>
         <img src="" className="icon-draw active" style={{ WebkitMask: 'url("/Icons/icon-08.svg") center center no-repeat' }} />
         <p className='text-body-project'>Click on the icon above and draw a polygon to define the project feature</p>
-      </div>
+      </div>}
       <Table
         dataSource={dataSourceGeomeotry}
         columns={columnsGeomeotry}
