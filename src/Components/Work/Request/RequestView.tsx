072b73bd15aadb4283a562eff3328d191e49c604
Diego <diego@vizonomy.com>
2023-08-04 11:19:13 -0400

    Litle fixes and disguise

--- src/Components/Work/Request/RequestView.tsx
+++ src/Components/Work/Request/RequestView.tsx
@@ -423,14 +423,14 @@ return (
                 <div className='button-header-tab'>
                   <YearDropdown />
                   <div className='button-header'>
-                    <Button id='buttons-header' style={selectView === 'card' && widthMap === 15 ? {display:'none'}:{}} className={selectView === 'list' ? 'ico-header-tab-active' : 'ico-header-tab'} onClick={() => { selectCard('list', true) }}>
+                    {/* <Button id='buttons-header' style={selectView === 'card' && widthMap === 15 ? {display:'none'}:{}} className={selectView === 'list' ? 'ico-header-tab-active' : 'ico-header-tab'} onClick={() => { selectCard('list', true) }}>
                       {selectView === 'list' ? <img src='Icons/ic-list-purple.svg' alt='ic-list-purple' /> : <img src='Icons/ic-list.svg' alt='ic-list' />}
                       List
                     </Button>
                     <Button id='buttons-header'  style={selectView === 'list' && widthMap === 15 ? {display:'none'}:{}} className={selectView === 'card' ? 'ico-header-tab-active' : 'ico-header-tab'} onClick={() => { selectCard('card', false) }}>
                       {selectView === 'card' ? <img src='Icons/ic-card-purple.svg' alt='ic-card-purple' /> : <img src='Icons/ic-card.svg' alt='ic-card' />}
                       Card
-                    </Button>
+                    </Button> */}
                   </div>
                 </div>
                 </Col>
