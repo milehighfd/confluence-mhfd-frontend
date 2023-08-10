173b7cbea2c418a626f294bfee4effb276859bda
Donwilo <burgoadanilson@gmail.com>
2023-08-10 16:38:53 -0400

    disable checks when sa or county in main dropdown

--- src/Components/Work/Drawers/FilterGroup.tsx
+++ src/Components/Work/Drawers/FilterGroup.tsx
@@ -1,15 +1,17 @@
 import { InfoCircleOutlined } from '@ant-design/icons';
 import { Checkbox } from 'antd';
-import { useRequestDispatch } from 'hook/requestHook';
+import { useRequestDispatch, useRequestState } from 'hook/requestHook';
 import React from 'react';
 import { capitalLetter, capitalizeWords } from 'utils/utils';
 
 const FilterGroup = ({
   label,
   filterList,
+  disabled = false
 }: {
   label: string;
   filterList: any[];
+  disabled?: boolean;
 }) => {
   const {
     toggleFilter
@@ -24,7 +26,7 @@ const FilterGroup = ({
         {
           filterList?.map((cn: any, index: number) => (
             <div key={`filter-ps${index}`} className='filter-00'>
-              <Checkbox className='check-filter' checked={cn?.selected} onChange={e => {
+              <Checkbox disabled={disabled} className='check-filter' checked={cn?.selected} onChange={e => {
                 toggleFilter(cn?.type,cn?.id)
               }} />
               &nbsp;&nbsp;&nbsp;{cn?.name}
