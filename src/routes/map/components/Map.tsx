ca9a43776b9b1104b944d72c4b39093b964c8058
cesarLima1 <cesmaulim@gmail.com>
2023-08-18 15:33:30 -0400

    remove logs

--- src/routes/map/components/Map.tsx
+++ src/routes/map/components/Map.tsx
@@ -889,8 +889,6 @@ const Map = ({ leftWidth, commentVisible, setCommentVisible }: MapProps) => {
               [Number(verifiedStatus)]: newValues,
             },
           };
-          console.log('groupedIdsBoardProjects', groupedIdsBoardProjects)
-          console.log('result ids grouped bwfore split ', result);
           // delete result.undefined;
           let idsToFilter: any = [];
           typeLayer?.forEach((type: any) => {
