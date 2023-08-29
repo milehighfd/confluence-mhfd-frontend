8971e2b5b2c972ce11ec597908b61d3e83c84efa
Jose Laura <pachonjcl@gmail.com>
2023-08-29 09:38:47 -0400

    fix: year dropdown

--- src/routes/work-request/components/YearDropdown.tsx
+++ src/routes/work-request/components/YearDropdown.tsx
@@ -1,24 +1,13 @@
 import React, { useState } from 'react';
 import { Select } from 'antd';
 import { useRequestDispatch, useRequestState } from 'hook/requestHook';
-import { GOVERNMENT_STAFF, WINDOW_WIDTH, WORK_PLAN } from 'constants/constants';
+import { WINDOW_WIDTH } from 'constants/constants';
 import { setIsLocatedInSouthPlateRiverSelected } from 'store/actions/requestActions';
-import { useProfileState } from 'hook/profileHook';
-import { useMapState } from 'hook/mapHook';
 
 const YearDropdown = () => {
   const { year, yearList } = useRequestState();
   const { setYear, setPrioritySelected } = useRequestDispatch();
   const [openYearDropdown, setOpenYearDropdown] = useState(false);
-  const {
-    isLocalGovernment,
-    userInformation: {
-      designation
-    }
-  } = useProfileState();
-  const { tabActiveNavbar } = useMapState();
-
-  const shouldHideCurrentYear = (isLocalGovernment || designation === GOVERNMENT_STAFF) && tabActiveNavbar === WORK_PLAN;
 
   return (
     <Select
@@ -35,12 +24,6 @@ const YearDropdown = () => {
       className={'ant-select-2'}>
       {
         yearList
-          .filter((_: number, i: number) => {
-            if (shouldHideCurrentYear) {
-              return i !== 0;
-            }
-            return true;
-          })
           .map((y: number, i: number) => (
           <Select.Option key={i} value={y} className="custom-option-background">
           {y}
