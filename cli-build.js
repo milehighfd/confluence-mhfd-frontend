5c6e5ee15278488b969be35ce69c4f2b49df65f9
Donwilo <burgoadanilson@gmail.com>
2023-08-30 16:28:50 -0400

    copy env to env production before build

--- cli-build.js
+++ cli-build.js
@@ -3,6 +3,7 @@
 const program = require('commander');
 const shell = require('shelljs');
 const fs = require('fs-extra');
+const path = require('path');
 const buildOutputFolder = 'build';
 program
   .version('1.0.0')
@@ -10,14 +11,28 @@ program
   .option('-e, --env <environment>', 'Specify the environment (development, production, test)')
   .parse(process.argv);
 
-const validEnvironments = ['production', 'dev2', 'test'];
+const validEnvironments = ['prod', 'dev2', 'test'];
 const selectedEnv = program.opts().env;
 
 const runBuild = (env) => {
+  // Update .env.production file or generate a new one based on the selected environment
   console.log(`Building for ${env} environment...`);
+  
+  const sourceEnvPath = path.join(__dirname, `.env.${env}`);
+  const destEnvPath = path.join(__dirname, '.env.production');
+  
+  // Check if the source env file exists
+  if (fs.existsSync(sourceEnvPath)) {
+    // Copy the source env file to .env.production
+    fs.copyFileSync(sourceEnvPath, destEnvPath);
+    console.log(`Environment file ${sourceEnvPath} copied to ${destEnvPath}`)
+  } else {
+    console.error(`No environment file found for ${env}.`);
+    process.exit(1);
+  }
 
   // Run build command with selected environment file
-  const buildCommand = `npm run build -- --env=env.${env}`;
+  const buildCommand = `npm run build -- --env=${env}`;
   const result = shell.exec(buildCommand);
 
   if (result.code !== 0) {
@@ -35,10 +50,20 @@ if (!selectedEnv || !([...validEnvironments, 'all']).includes(selectedEnv)) {
 
 if (selectedEnv != 'all') {
   runBuild(selectedEnv);
+  if (!fs.existsSync(selectedEnv)) {
+    fs.mkdirSync(selectedEnv);
+  }else {
+    fs.removeSync(selectedEnv);
+    fs.mkdirSync(selectedEnv);
+  }
+  fs.copySync(buildOutputFolder, selectedEnv + '/' + buildOutputFolder);
 } else {
   function createBuildFolders() {
     validEnvironments.forEach(environment => {
       if (!fs.existsSync(environment)) {
+        fs.mkdirSync(environment);
+      }else {
+        fs.removeSync(environment);
         fs.mkdirSync(environment);
       }
     });
