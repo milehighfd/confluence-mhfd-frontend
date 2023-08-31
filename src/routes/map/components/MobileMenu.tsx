16e1a1dd2bfb7952509cde73931c70806194f43c
Diego <diego@vizonomy.com>
2023-08-31 14:15:04 -0400

    Mobile Cards problems, proyects and filter layers

--- src/routes/map/components/MobileMenu.tsx
+++ src/routes/map/components/MobileMenu.tsx
@@ -3,7 +3,8 @@ import { Button, Checkbox, Col, Collapse, Popover, Row, Tabs } from 'antd';
 import GenericTabView from 'Components/Shared/GenericTab/GenericTabView';
 import { useMapDispatch, useMapState } from 'hook/mapHook';
 import { FILTER_PROBLEMS_TRIGGER, FILTER_PROJECTS_TRIGGER, PROBLEMS_TRIGGER, PROJECTS_TRIGGER } from 'routes/map/constants/tabs.constants';
-import { getCurrentProjectStatus } from 'utils/parsers';
+import { getCurrentProjectStatus, getMainSponsor } from 'utils/parsers';
+import { SERVER } from 'Config/Server.config';
 
 let contents: any = [];
 contents.push((<div className="popoveer-00"><b>Problems:</b> Problems represent areas where values such as public health, safety, and environmental quality are at risk due to potential flooding, erosion, or other identified threats within MHFD’s purview.</div>));
@@ -16,6 +17,7 @@ const MobileMenu = () => {
   const {
     applyFilter,
     galleryProblems,
+    galleryProjectsV2,
     galleryProjects
   } = useMapState();
   const {
@@ -78,7 +80,7 @@ const MobileMenu = () => {
                 } else {
                   setTabCards(PROJECTS_TRIGGER);
                                        console.log('get gallery'); 
-                      getGalleryProjects();;
+                      getGalleryProjects();
                 }
               }} activeKey={tabPosition} onChange={(key) => setTabPosition(key)} className="tabs-map over-00" tabBarExtraContent={genExtra()}>
                 {tabs.map((value: string, index: number) => {
@@ -92,52 +94,86 @@ const MobileMenu = () => {
                         requestName: problem.problemname,
                         jurisdiction: problem.jurisdiction,
                         estimatedCost: problem.estimatedcost,
+                        componentCost: problem.component_cost ? problem.component_cost : 0,
                         field4: 'X',
                         field5: 'Components',
+                        problemtype: problem.problemtype,
                         priority: problem.problempriority,
-                        percentage: problem.solutionstatus,
+                        percentage: problem.solutionstatus ? problem.solutionstatus : 0,
                         problemid: problem.problemid,
                         type: problem.type,
                         value: problem.cartodb_id,
                         totalComponents: problem.totalComponents,
-                        coordinates: problem.coordinates[0]
-                      }
+                        coordinates: problem.coordinates[0],
+                        count: problem.count,
+                      };
                     });
                     totalElements = cardInformation.length;
                   } else {
-                    cardInformation = galleryProjects.map((project: any) => {
-                      return {
-                        cartodb_id: project.cartodb_id,
-                        image: project.attachments ? project.attachments : (
-                          project.projecttype === 'Capital' ? '/projectImages/capital.png' :
-                            project.projecttype === 'Study' ? '/projectImages/study.png' :
-                              project.projecttype === 'Maintenance' ?
-                                (project.projectsubtype === 'Vegetation Management' ? '/projectImages/vegetation-management.png' :
-                                  project.projectsubtype === 'Sediment Removal' ? '/projectImages/sediment-removal.png' :
-                                    project.projectsubtype === 'Restoration' ? '/projectImages/restoration.png' :
-                                      project.projectsubtype === 'Minor Repairs' ? '/projectImages/minor-repairs.png' :
-                                        '/projectImages/debris_management.png') : '/Icons/eje.png'
-                        ),
-                        requestName: project.projectname ? project.projectname : project.requestedname,
-                        sponsor: project.sponsor,
-                        estimatedCost: project.finalcost ? project.finalcost : project.estimatedcost,
+                    cardInformation = galleryProjectsV2?.projects?.map((project: any) => {
+                      const projectType = project?.code_project_type?.project_type_name;
+                      const x = {
+                        cartodb_id: project.project_id,
+                        project_id: project.project_id,
+                        image:
+                        project?.project_attachments?.length > 0 ? `${SERVER.BASE_URL_IMAGES}/${project?.project_attachments[0]?.attachment_url}`:
+                            projectType === 'CIP'
+                            ? '/projectImages/capital.png'
+                            : projectType === 'Study'
+                            ? '/projectImages/study.png'
+                            : projectType === 'Special'
+                            ? '/projectImages/special.png'
+                            : projectType === 'Vegetation Management'
+                            ? '/projectImages/vegetation-management.png'
+                            : projectType === 'Sediment Removal'
+                            ? '/projectImages/sediment-removal.png'
+                            : projectType === 'Restoration'
+                            ? '/projectImages/restoration.png'
+                            : projectType === 'General Maintenance'
+                            ? '/projectImages/minor-repairs.png'
+                            : projectType === 'Acquisition'
+                            ? '/projectImages/acquisition.png'
+                            : projectType === 'Routine Trash and Debris'
+                            ? '/projectImages/debris-management.png'
+                            : '/projectImages/watershed-change.png',
+                        requestName: project.project_name,
+                        sponsor: getMainSponsor(project.project_partners),
+                        project_costs: project.project_costs,
+                        estimatedCost: project.estimatedcost ? project.estimatedcost : project.finalcost,
+                        componentCost: project.component_cost ? project.component_cost : 0,
                         status: getCurrentProjectStatus(project)?.code_phase_type?.code_status_type?.status_name,
-                        projecttype: project.projecttype,
+                        projecttype: projectType,
                         objectid: project.objectid,
                         type: project.type,
                         value: project.cartodb_id,
-                        id: project.projectid,
-                        totalComponents: project.totalComponents,
-                        coordinates: project.coordinates[0]
-                      }
+                        id: project.projectId,
+                        phase: project?.currentId[0]?.code_phase_type?.phase_name,
+                        stream: project?.project_streams,
+                        totalComponents: parseInt(
+                          project.GRADE_CONTROL_STRUCTURE +
+                          project.PIPE_APPURTENANCES +
+                          project.SPECIAL_ITEM_POINT +
+                          project.SPECIAL_ITEM_LINEAR +
+                          project.SPECIAL_ITEM_AREA +
+                          project.CHANNEL_IMPROVEMENTS_LINEAR +
+                          project.CHANNEL_IMPROVEMENTS_AREA +
+                          project.REMOVAL_LINE +
+                          project.REMOVAL_AREA +
+                          project.STORM_DRAIN +
+                          project.DETENTION_FACILITIES +
+                          project.MAINTENANCE_TRAILS +
+                          project.LAND_ACQUISITION +
+                          project.LANDSCAPING_AREA ),
+                      };
+                      return x;
                     });
-                    totalElements = cardInformation.length;
+                    totalElements = galleryProjectsV2?.count ?? 0;
                   }
 
                   return (
                     <Tabs.TabPane tab={<span><Popover content={contents[index]} placement="rightBottom">{value} </Popover> </span>} key={'' + index}>
                       <GenericTabView
-                        key={value + index}
+                        //key={value + index}
                         type={value}
                         totalElements={totalElements}
                         cardInformation={cardInformation}
