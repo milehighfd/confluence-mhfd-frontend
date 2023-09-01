09794a6136a75aa23cfda0fa14819ef72933330d
Donwilo <burgoadanilson@gmail.com>
2023-09-01 16:15:48 -0400

    Fix notification showing with no data inside

--- src/Components/Project/Capital/ModalCapital.tsx
+++ src/Components/Project/Capital/ModalCapital.tsx
@@ -594,11 +594,13 @@ export const ModalCapital = ({
           if (selectedTypeProject === 'acquisition' && !purchaseDate) missingFields.push('Purchase Date');
           if (selectedTypeProject === 'maintenance' && !frequency) missingFields.push('Frequency');
           if (selectedTypeProject === 'maintenance' && !eligibility) missingFields.push('Eligibility');
+          if (thisIndependentComponents.length && !checkIfIndependentHaveName()) {
+            missingFields.push('Independent component name');
+          }
           if (missingFields.length > 0) {
             handleErrorNotification(missingFields);
-          }
-          if (thisIndependentComponents.length && !checkIfIndependentHaveName()) {
-            handleNotification(`Independent components should have a name.`);
+          }else{
+            handleNotification('Please fill out all required fields.');
           }
         }
       }
