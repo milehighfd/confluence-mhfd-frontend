cc7d1835467ff711e86fa283043c747ff39f1ee9
cesarLima1 <cesmaulim@gmail.com>
2023-09-05 13:58:00 -0400

    continue adding new graph

--- src/routes/detail-page/components/StackedBarChart.tsx
+++ src/routes/detail-page/components/StackedBarChart.tsx
@@ -3,25 +3,25 @@ import * as d3 from 'd3';
 
 const StackedBarChart = ({}) => {
   const [maxValue, setMaxValue] = useState(0);
+  const [tickValues, setTickValues] = useState([]);
   const svgRef = useRef<SVGSVGElement>(null);
   const totalWidth:any = document.getElementById('ProjectRoadmapHeader')?.clientWidth;
-  console.log('totalWidth', totalWidth)
   // const totalWidth = 260;
   const totalHeight = 200;
   const margin = {top: 10, right: 30, bottom: 20, left: 50},
         width = totalWidth - margin.left - margin.right,
         height = totalHeight - margin.top - margin.bottom;
   const data = [
-    {group: 'banana', Nitrogen: '12', normal: '1', stress: '13'},
-    {group: 'poacee', Nitrogen: '6', normal: '6', stress: '33'},
-    {group: 'sorgho', Nitrogen: '11', normal: '28', stress: '12'},
-    {group: 'triticum', Nitrogen: '19', normal: '6', stress: '1'},
-    {group: 'banana1', Nitrogen: '12', normal: '1', stress: '13'},
-    {group: 'poacee2', Nitrogen: '6', normal: '6', stress: '33'},
-    {group: 'sorgho3', Nitrogen: '11', normal: '28', stress: '12'},
-    {group: 'triticum3', Nitrogen: '19', normal: '6', stress: '1'}
+    {group: 'banana', Nitrogen: '12', normal: '1', stress: '13' , additional: '12'},
+    {group: 'poacee', Nitrogen: '6', normal: '6', stress: '33' , additional: '6'},
+    {group: 'sorgho', Nitrogen: '11', normal: '28', stress: '12' , additional: '3'},
+    {group: 'triticum', Nitrogen: '19', normal: '6', stress: '1' , additional: '30'},
+    {group: 'banana1', Nitrogen: '12', normal: '1', stress: '13' , additional: '12'},
+    {group: 'poacee2', Nitrogen: '6', normal: '6', stress: '33' , additional: '10'},
+    {group: 'sorgho3', Nitrogen: '11', normal: '28', stress: '12' , additional: '9'},
+    {group: 'triticum3', Nitrogen: '19', normal: '6', stress: '1' , additional: '3'}
   ];
-  const subGroups = ['Nitrogen', 'normal', 'stress'];
+  const subGroups = ['Nitrogen', 'normal', 'stress', 'additional'];
 
   useEffect(() => {
     const totals:any = {};
@@ -47,6 +47,8 @@ const StackedBarChart = ({}) => {
     console.log("Max Sum Group:", maxGroup);
     console.log("Max Sum:", maxSum);
     setMaxValue(maxSum)
+    const tickValue:any = Array.from({ length: Math.ceil(maxSum / 10) + 1 }, (_, i) => i * 10);
+    setTickValues(tickValue)
   }, []);
 
   useEffect(() => {
@@ -66,6 +68,12 @@ const StackedBarChart = ({}) => {
 
     svg.append("g")
       .attr("transform", `translate(0, ${height})`)
+      .attr('class', 'x-axis-stackedbar-chart')
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .attr('class', 'x-axis-stackedbar-chart-for-background')
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
     // Add Y axis
@@ -74,12 +82,12 @@ const StackedBarChart = ({}) => {
       .range([ height, 0 ]);
     svg.append("g")
       .attr('class', 'y-axis-stackedbar-chart')
-      .call(d3.axisLeft(y));
+      .call(d3.axisLeft(y).tickValues(tickValues));
 
     // color palette = one color per subgroup
     const color = d3.scaleOrdinal()
       .domain(subGroups)
-      .range(['#e41a1c','#377eb8','#4daf4a'])
+      .range(['#5D3DC7','#047CD7','#29C499','#F4BE01'])
 
     //stack the data? --> stack per subgroup
     const stackedData = d3.stack()
@@ -100,8 +108,9 @@ const StackedBarChart = ({}) => {
           .attr("y", (d:any): any => { return y(d[1]); })
           .attr("height", (d:any) => { const dimention = (y(d[0]) ?? 0) - (y(d[1]) ?? 0); return dimention; })
           .attr("width",x.bandwidth())
-          .attr("rx", 1.5)
+          .attr("rx", 5)
           .attr('stroke', 'white')
+          .attr('stroke-width', '2')
           .style('stroke-linecap', 'round')
   } ,[data]);
   return (
