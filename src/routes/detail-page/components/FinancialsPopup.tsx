270522c80a6472769c5983c9d0475ea7afbfcaa9
cesarLima1 <cesmaulim@gmail.com>
2023-09-06 15:53:41 -0400

    add prettier to code

--- src/routes/detail-page/components/FinancialsPopup.tsx
+++ src/routes/detail-page/components/FinancialsPopup.tsx
@@ -1,69 +1,69 @@
-import { Col, Row } from "antd";
-import React, { useEffect, useRef, useState } from "react";
+import { Col, Row } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
 
-const FinancialsPopup = ({popupData}:{popupData:any}) => {
-    let positionTop: any = document.getElementById('stackedBar-chart-container')?.offsetTop;
-    let positionLeft: any = document.getElementById('stackedBar-chart-container')?.offsetLeft;
-    let divWidth: any = document.getElementById('stackedBar-chart-container')?.offsetWidth;
-    let thisPopup = useRef<any>(null);
-    const [popupWidth, setPopupWidth] = useState(0);
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0
-    });
-    const [finalLeftPosition, setFinalLeftPosition] = useState(positionLeft + divWidth - 2000);
-    useEffect(() => {
-      setPopupWidth(thisPopup.current.offsetWidth);
-    }, []);
-    useEffect(() => {
-      setFinalLeftPosition(positionLeft + divWidth - popupWidth);
-    } ,[popupWidth]);
+const FinancialsPopup = ({ popupData }: { popupData: any }) => {
+  let positionTop: any = document.getElementById('stackedBar-chart-container')?.offsetTop;
+  let positionLeft: any = document.getElementById('stackedBar-chart-container')?.offsetLeft;
+  let divWidth: any = document.getElementById('stackedBar-chart-container')?.offsetWidth;
+  let thisPopup = useRef<any>(null);
+  const [popupWidth, setPopupWidth] = useState(0);
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+  const [finalLeftPosition, setFinalLeftPosition] = useState(positionLeft + divWidth - 2000);
+  useEffect(() => {
+    setPopupWidth(thisPopup.current.offsetWidth);
+  }, []);
+  useEffect(() => {
+    setFinalLeftPosition(positionLeft + divWidth - popupWidth);
+  }, [popupWidth]);
 
-  return(
+  return (
     <div
-      className='modal-financialsPopup'
-      id='popup-financials'
-      style={{left:finalLeftPosition, top:positionTop}}
+      className="modal-financialsPopup"
+      id="popup-financials"
+      style={{ left: finalLeftPosition, top: positionTop }}
       ref={thisPopup}
     >
       <p className="title">{popupData.group}</p>
-    <Row>
+      <Row>
         <Col className="row-financials-popup">
-            <Row>
-                <span className="span-dots-roadmap">
-                <div className="roadmap-circle" style={{ backgroundColor: '#5D3DC7' }} />
-                <span className='labels-financials'>Funding</span>
-                </span>
-            </Row>
-            <Row>
-                <span className="span-dots-roadmap">
-                <div className="roadmap-circle" style={{ backgroundColor: '#047CD7' }} />
-                <span className='labels-financials'>Income</span>
-                </span>
-            </Row>
-            <Row>
-                <span className="span-dots-roadmap">
-                <div className="roadmap-circle" style={{ backgroundColor: '#29C499' }} />
-                <span className='labels-financials'>Agreement</span>
-                </span>
-            </Row>
+          <Row>
+            <span className="span-dots-roadmap">
+              <div className="roadmap-circle" style={{ backgroundColor: '#5D3DC7' }} />
+              <span className="labels-financials">Funding</span>
+            </span>
+          </Row>
+          <Row>
+            <span className="span-dots-roadmap">
+              <div className="roadmap-circle" style={{ backgroundColor: '#047CD7' }} />
+              <span className="labels-financials">Income</span>
+            </span>
+          </Row>
+          <Row>
+            <span className="span-dots-roadmap">
+              <div className="roadmap-circle" style={{ backgroundColor: '#29C499' }} />
+              <span className="labels-financials">Agreement</span>
+            </span>
+          </Row>
         </Col>
-        <Col className="row-financials-popup" style={{paddingLeft: '35px'}}>
-            <Row>
-                <span className='labels-financials-numbers'>{formatter.format(popupData.funding)}</span>
-            </Row>
-            <Row>
-                <span className='labels-financials-numbers'>{formatter.format(popupData.income)}</span>
-            </Row>
-            <Row>
-                <span className='labels-financials-numbers'>{formatter.format(popupData.agreement)}</span>
-            </Row>
+        <Col className="row-financials-popup" style={{ paddingLeft: '35px' }}>
+          <Row>
+            <span className="labels-financials-numbers">{formatter.format(popupData.funding)}</span>
+          </Row>
+          <Row>
+            <span className="labels-financials-numbers">{formatter.format(popupData.income)}</span>
+          </Row>
+          <Row>
+            <span className="labels-financials-numbers">{formatter.format(popupData.agreement)}</span>
+          </Row>
         </Col>
-    </Row>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
 export default FinancialsPopup;
--- src/routes/detail-page/components/StackedBarChart.tsx
+++ src/routes/detail-page/components/StackedBarChart.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import FinancialsPopup from './FinancialsPopup';
 
-const StackedBarChart = ({}) => {
+const StackedBarChart = () => {
   const [openPopup, setOpenPopup] = useState(false);
   const [dataPopup, setDataPopup] = useState({});
-  // const dataPopup = useRef<any>({});
   const svgRef = useRef<SVGSVGElement>(null);
   const barWidth = 60;
   const totalHeight = 350;
@@ -15,6 +14,7 @@ const StackedBarChart = ({}) => {
   const totalWidth = barWidth * data.length + margin.left + margin.right;
   const width = totalWidth - margin.left - margin.right;
   const height = totalHeight - margin.top - margin.bottom;
+
   useEffect(() => {
     setData([
       {group: 'banana', funding: '12', income: '1', agreement: '13' , additional: '12'},
@@ -46,7 +46,6 @@ const StackedBarChart = ({}) => {
 
 
   const applyBackgroundRect = (type: string, x: any, y:any, d: any, backgroundRect: any, sumGroups: any) => {
-    console.log('Type of apply react', type);
     if (type === 'add') {
       backgroundRect
       .attr('y',  y(sumGroups[d.data.group]) - 7)
@@ -61,127 +60,124 @@ const StackedBarChart = ({}) => {
       backgroundRect.attr('class', 'background-rect-hidden').attr("id", 'rect-undefined');
       d3.select(`#${d.data.group}`).attr('class', 'x-axis-stackedbar-chart text');  
     }
-    
   }
   const buildChart = (dataChart: any) => {
     const totals:any = {};
       data.forEach((item:any) => {
-          if (!totals[item.group]) {
-              totals[item.group] = 0;
-          }
-          subGroups.forEach((subGroup:any) => {
-              totals[item.group] += parseInt(item[subGroup]);
-          });
+        if (!totals[item.group]) {
+          totals[item.group] = 0;
+        }
+        subGroups.forEach((subGroup:any) => {
+          totals[item.group] += parseInt(item[subGroup]);
+        });
       });
     let maxValue;
     let tickValues;
     let sumGroups: any;
     let maxSum = -Infinity;
-    let maxGroup = '';
     for (const group in totals) {
         if (totals[group] > maxSum) {
             maxSum = totals[group];
-            maxGroup = group;
         }
     }
     sumGroups = totals;
     maxValue = maxSum;
     const tickValue:any = Array.from({ length: Math.ceil(maxSum / 10) + 1 }, (_, i) => i * 10);
     tickValues = tickValue;
+
     d3.select(svgRef.current).select('.stackedBar-chart').remove();
-      const svg = d3.select(svgRef.current)
-        .attr('width', totalWidth)
-        .attr('height', totalHeight)
-        // .attr("viewBox", `0 0 ${totalWidth} ${totalHeight} `)
-        .append("g")
-        .attr("id", "stackedBar-chart")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      const groups = dataChart.map((d: any) => (d.group));
-      const x = d3.scaleBand()
-        .domain(groups)
-        .range([0, width])
-        .padding(0.15);
+    const svg = d3.select(svgRef.current)
+      .attr('width', totalWidth)
+      .attr('height', totalHeight)
+      .append("g")
+      .attr("id", "stackedBar-chart")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    const groups = dataChart.map((d: any) => (d.group));
+    const x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding(0.15);
+
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .attr('class', 'x-axis-stackedbar-chart')
+      .call(d3.axisBottom(x).tickSizeOuter(0));
   
-      svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .attr('class', 'x-axis-stackedbar-chart')
-        .call(d3.axisBottom(x).tickSizeOuter(0));
-  
-      svg.append("g")
-        .attr("transform", `translate(0, ${height})`)
-        .attr('class', 'x-axis-stackedbar-chart-for-background')
-        .call(d3.axisBottom(x).tickSizeOuter(0));
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .attr('class', 'x-axis-stackedbar-chart-for-background')
+      .call(d3.axisBottom(x).tickSizeOuter(0));
   
       // Add Y axis
-      const y: any = d3.scaleLinear()
-        .domain([0, maxValue])
-        .range([ height, 0 ]);
-      svg.append("g")
-        .attr('class', 'y-axis-stackedbar-chart')
-        .call(d3.axisLeft(y).tickValues(tickValues));
-  
+    const y: any = d3.scaleLinear()
+      .domain([0, maxValue])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .attr('class', 'y-axis-stackedbar-chart')
+      .call(d3.axisLeft(y).tickValues(tickValues));
+
       // color palette = one color per subgroup
-      const color = d3.scaleOrdinal()
-        .domain(subGroups)
-        .range(['#5D3DC7','#047CD7','#29C499','#F4BE01']);
+    const color = d3.scaleOrdinal()
+      .domain(subGroups)
+      .range(['#5D3DC7','#047CD7','#29C499','#F4BE01']);
   
-      d3.select(".x-axis-stackedbar-chart").selectAll("text").attr("id", function(d:any) { return d});
+    d3.select(".x-axis-stackedbar-chart").selectAll("text").attr("id", function(d:any) { return d});
   
-      //stack the data? --> stack per subgroup
-      const stackedData = d3.stack()
-        .keys(subGroups)
-        (dataChart as any);
-      console.log('Stacked data', stackedData);
-      const backgroundRect = svg
-          .append("g")
-          .append('rect')
-          .attr('y',  150)
-          .attr('height',  0)
-          .attr('x',  100)
-          .attr('width', 0)
-          .attr("rx", 5)
-          .attr("id", 'background-rect');
+    //stack the data? --> stack per subgroup
+    const stackedData = d3.stack()
+      .keys(subGroups)
+      (dataChart as any);
+    const backgroundRect = svg
+      .append("g")
+      .append('rect')
+      .attr('y',  150)
+      .attr('height',  0)
+      .attr('x',  100)
+      .attr('width', 0)
+      .attr("rx", 5)
+      .attr("id", 'background-rect');
       // Show the bars
-      svg.append("g")
-        .selectAll("g")
-        .data(stackedData)
-        .enter().append("g")
-          .attr("fill", (d: any): any => { return color(d.key); })
-          .selectAll("rect")
-          // enter a second time = loop subgroup per subgroup to add all rectangles
-          .data((d: any): any => { return d; })
-          .enter().append("rect")
-            .attr("x", (d:any):any => { return x(d.data.group); })
-            .attr("y", (d:any): any => { return y(d[1]); })
-            .attr("height", (d:any) => { const dimention = (y(d[0]) ?? 0) - (y(d[1]) ?? 0); return dimention; })
-            .attr("width", x.bandwidth())
-            .attr("rx", 5)
-            .attr('stroke', 'white')
-            .attr('stroke-width', '2')
-            .style('stroke-linecap', 'round')
-            .on("click", (d: any) => {
-              const selection = d3.select(`#rect-${d.data.group}`);
-              console.log('Selection', selection, 'value', !selection.empty());
-              if (!selection.empty()) { // true the rect is already there // false the rect is of other or doesnt exist 
-                applyBackgroundRect('remove', x , y, d, backgroundRect, sumGroups);
-                setOpenPopup(false);
-              } else {
-                d3.select('.x-axis-selected').attr('class', 'x-axis-stackedbar-chart text');
-                applyBackgroundRect('add', x , y, d, backgroundRect, sumGroups);
-                setDataPopup(d.data);
-                setOpenPopup(true);
-              }
-            });
+    svg.append("g")
+      .selectAll("g")
+      .data(stackedData)
+      .enter().append("g")
+        .attr("fill", (d: any): any => { return color(d.key); })
+        .selectAll("rect")
+        // enter a second time = loop subgroup per subgroup to add all rectangles
+        .data((d: any): any => { return d; })
+        .enter().append("rect")
+          .attr("x", (d:any):any => { return x(d.data.group); })
+          .attr("y", (d:any): any => { return y(d[1]); })
+          .attr("height", (d:any) => { const dimention = (y(d[0]) ?? 0) - (y(d[1]) ?? 0); return dimention; })
+          .attr("width", x.bandwidth())
+          .attr("rx", 5)
+          .attr('stroke', 'white')
+          .attr('stroke-width', '2')
+          .style('stroke-linecap', 'round')
+          .on("click", (d: any) => {
+            const selection = d3.select(`#rect-${d.data.group}`);
+            if (!selection.empty()) { // true the rect is already there // false the rect is of other or doesnt exist 
+              applyBackgroundRect('remove', x , y, d, backgroundRect, sumGroups);
+              setOpenPopup(false);
+            } else {
+              d3.select('.x-axis-selected').attr('class', 'x-axis-stackedbar-chart text');
+              applyBackgroundRect('add', x , y, d, backgroundRect, sumGroups);
+              setDataPopup(d.data);
+              setOpenPopup(true);
+            }
+          });
   }
+
   useEffect(() => {
     if(data.length) {
       buildChart(data);
     }
   } ,[data]);
+  
   return (
     <>
       { openPopup && <FinancialsPopup popupData={dataPopup}/>}
-      <div id='stackedBar-chart-container' style={{overflowY: 'auto', position: 'relative'}}>
+      <div id='stackedBar-chart-container' style={{overflowY: 'auto', position: 'relative', marginTop: '50px', marginBottom: '10px'}}>
         <svg ref={svgRef} width="100%" height="100%" />
       </div>
       <div className='roadmap-body-display ' style={{ paddingTop: '0px' }}>
