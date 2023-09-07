c0efe68316060642ae25d1d3ae82677a0eb975c0
Diego <diego@vizonomy.com>
2023-09-07 12:34:13 -0400

    Search NavBar

---
+++ src/Components/Shared/Navbar/NavBarSearchTooltip.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const NavBarSearchTooltip = () => {
+  return (
+    <div className='navbar-search-tooltip'>
+      <div className='navbar-search-tooltip-item'>
+        <h1>PM Tools &gt; Detail Page</h1>
+        <div className='navbar-search-tooltip-card'>
+          <h2>West Tollgate Creek @ 15th and Clemtine 15th and Clemtine 15th and Clemtine</h2>
+          <div>
+            <p>CIP Project · Construction Phase</p>
+            <span>Draft</span>
+          </div>
+        </div>
+      </div>
+      <div className='navbar-search-tooltip-item'>
+        <h1>Work Request</h1>
+        <div className='navbar-search-tooltip-card'>
+          <h2>West Tollgate Creek @ 15th and Clemtine 15th and Clemtine 15th and Clemtine</h2>
+          <div>
+            <p>2023 Work Request · R&D</p>
+            <span>Draft</span>
+          </div>
+        </div>
+        <div className='navbar-search-tooltip-card'>
+          <h2>West Tollgate Creek @ 15th and Clemtine 15th and Clemtine 15th and Clemtine</h2>
+          <div>
+            <p>2023 Work Request · Maintenance</p>
+            <span>Draft</span>
+          </div>
+        </div>
+      </div>
+      <div className='navbar-search-tooltip-item'>
+        <h1>Work Plan</h1>
+        <div className='navbar-search-tooltip-card'>
+          <h2>West Tollgate Creek @ 15th and Clemtine 15th and Clemtine 15th and Clemtine</h2>
+          <div>
+            <p>2023 Work Plan · Acquisition</p>
+            <span>Draft</span>
+          </div>
+        </div>
+        <div className='navbar-search-tooltip-card'>
+          <h2>West Tollgate Creek @ 15th and Clemtine 15th and Clemtine 15th and Clemtine</h2>
+          <div>
+            <p>2023 Work Plan · Study</p>
+            <span>Draft</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBarSearchTooltip;
