941ac977be34fd76ad48290a69f0e9bdec2b603a
Dotty <dottykurmi123@gmail.com>
2023-11-13 12:31:13 -0400

    edit style 2560 and 1920 in table board year

--- src/routes/user-management/components/BoardYear.tsx
+++ src/routes/user-management/components/BoardYear.tsx
@@ -209,14 +209,17 @@ const BoardYear = () => {
 
   return (
     <div>
+      <div style={{position: 'absolute', top: '95px', right: '23px'}} className='img-dowload'>
+        <img src='/Icons/ic-dowload.svg' alt='dowload'/>
+      </div>
       {contextHolder}
       <Row>
-        <Col xs={{ span: 24}} lg={{ span: 24 }} style={{paddingLeft: '37px', paddingRight:'37px'}}>
+        <Col xs={{ span: 24}} lg={{ span: 24 }} className='col-table-board' style={{paddingLeft: '2px', paddingRight:'2px'}}>
           <div className="list-view-head" style={{paddingTop:'10px', }} >
             <h2 style={{color:'rgb(29, 22, 70)'}}className="title">Board Access</h2>
           </div>
           <div style={{ }} >
-            <p>These settings limit which boards are available and set as the default , according to user type. </p>
+            <p className='board-table-year-p' style={{color: '#11093C', opacity:0.6}}>These settings limit which boards are available and set as the default , according to user type. </p>
           </div>
           <br />
           <div className='table-board-year'>
