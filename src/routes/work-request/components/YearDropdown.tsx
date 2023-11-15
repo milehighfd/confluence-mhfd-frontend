81d38d19ae31d254119abf94c9ad9cfe9bbc7653
Donwilo <burgoadanilson@gmail.com>
2023-11-15 15:54:28 -0400

    Load year for WR/WP according to user type

--- src/routes/work-request/components/YearDropdown.tsx
+++ src/routes/work-request/components/YearDropdown.tsx
@@ -1,29 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Select } from 'antd';
 import { useRequestDispatch, useRequestState } from 'hook/requestHook';
-import { WINDOW_WIDTH } from 'constants/constants';
+import { WINDOW_WIDTH, WORK_PLAN } from 'constants/constants';
 import { setIsLocatedInSouthPlateRiverSelected } from 'store/actions/requestActions';
+import { useProfileState } from 'hook/profileHook';
+import { useProjectState } from 'hook/projectHook';
 
 const YearDropdown = () => {
-  const { year, yearList } = useRequestState();
-  const { setYear, setPrioritySelected } = useRequestDispatch();
+  const { year, yearList, namespaceId } = useRequestState();
+  const { setYear, setPrioritySelected, setNamespaceId } = useRequestDispatch();
+  const { globalSearch } = useProjectState();
   const [openYearDropdown, setOpenYearDropdown] = useState(false);
+  const { workRequestYear, workPlanYear } = useProfileState();
+  const [defaultYear, setDefaultYear] = useState('2022');
+  const [yearListDropdown, setYearListDropdown] = useState<any>([]);
+
+  useEffect(() => {
+    const defaultValue = namespaceId.type === WORK_PLAN ? workPlanYear.default : workRequestYear.default;
+    if (!globalSearch){
+      setDefaultYear(defaultValue);
+      setYear(defaultValue);    
+      setNamespaceId({ ...namespaceId, year: defaultValue });
+    }else{
+      setDefaultYear(namespaceId.year);
+      setYear(namespaceId.year);
+    }
+    const maxYear = namespaceId.type === WORK_PLAN ? workPlanYear.max : workRequestYear.max;
+    const yearList = [];
+    for (let year = maxYear; year >= 2022; year--) {
+      yearList.push(year);
+    }
+    setYearListDropdown(yearList);
+  }, [namespaceId.type]);
 
   return (
     <Select
-      defaultValue={year}
-      value={`Year ${year}`}
+      defaultValue={defaultYear}
+      value={`Year ${defaultYear}`}
       showArrow={false}
       listHeight={WINDOW_WIDTH > 2554 ? (WINDOW_WIDTH > 3799 ? 500 : 320) : 256}
       onClick={() => (setOpenYearDropdown(!openYearDropdown))}
       onChange={(y: any) => {
+        setDefaultYear(y);
         setYear(y);
         setPrioritySelected(['1', '2', '3', 'Over 3', 'Work Plan']);
         setIsLocatedInSouthPlateRiverSelected([false]);
       }}
       className={'ant-select-2'}>
       {
-        yearList
+        yearListDropdown
           .map((y: number, i: number) => (
           <Select.Option key={i} value={y} className="custom-option-background">
           {y}
