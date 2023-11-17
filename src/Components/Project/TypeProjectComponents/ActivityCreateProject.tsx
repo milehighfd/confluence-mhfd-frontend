763c9aee5118a4206f72039ae21a0de2c1f4def4
cesarLima1 <cesmaulim@gmail.com>
2023-11-17 16:57:21 -0400

    fix

--- src/Components/Project/TypeProjectComponents/ActivityCreateProject.tsx
+++ src/Components/Project/TypeProjectComponents/ActivityCreateProject.tsx
@@ -73,7 +73,7 @@ export const ActivitiCreateProject = ({projectId, data}: {projectId: any, data: 
     });
     const mainproposedaction: any = historicProposedAction[0];
     let prefix = '';
-    let boldLegend = mainproposedaction.userModified=null ? `${mainproposedaction?.userModified?.firstName} ${mainproposedaction?.userModified?.lastName}`: `${mainproposedaction?.last_modified_by}` ;
+    let boldLegend = mainproposedaction.userModified !== null ? `${mainproposedaction?.userModified?.firstName} ${mainproposedaction?.userModified?.lastName}`: `${mainproposedaction?.last_modified_by}` ;
     const dateParsed = moment(mainproposedaction?.modified_date).format('MM/DD/YY');
     const hProposedActionValues = {
       date: moment(mainproposedaction?.modified_date),
