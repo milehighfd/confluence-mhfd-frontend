0522955f5a7f773cabb8481baa735a172afc430c
Diego <diego@vizonomy.com>
2023-12-11 14:52:36 -0400

    Upcoming Project Resolutions

--- src/routes/upcoming-projects/components/UpcomingProjectHeader.tsx
+++ src/routes/upcoming-projects/components/UpcomingProjectHeader.tsx
@@ -1,16 +1,19 @@
-import { AutoComplete, Select, Tag } from 'antd';
+import { Select } from 'antd';
 import React from 'react';
 
 const items = [
-  { value: 'jack', label: 'Jack' },
+  { value: 'South Park', label: 'South Park' },
   { value: 'South Watershed', label: 'South Watershed' },
-  { value: 'Yiminghe', label: 'yiminghe' },
+  { value: 'North Village', label: 'North Village' },
+  { value: 'East Primordial Sea', label: 'East Primordial Sea' },
+  { value: 'Wester Ice Land', label: 'Wester Ice Land' },
+  { value: 'Easter Fire Land', label: 'Easter Fire Land' },
 ];
 export const UpcomingProjectHeader = () => {
   return (
     <div className="upcoming-header">
       <Select className='upcoming-select' options={items} bordered={false} defaultValue='South Watershed'></Select>
-      <span className="upcoming-tag-label">Year 2023 - Quarter 2</span>
+      <span className="upcoming-tag-label">Year 2023</span>
     </div>
   );
 };
