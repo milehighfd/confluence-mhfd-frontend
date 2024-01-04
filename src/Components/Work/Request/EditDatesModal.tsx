97654c3605f1d45d87b0f6c1e8105708af0345c5
Diego <diego@vizonomy.com>
2024-01-04 13:10:48 -0400

    Fix align items Make active modal date

--- src/Components/Work/Request/EditDatesModal.tsx
+++ src/Components/Work/Request/EditDatesModal.tsx
@@ -551,13 +551,13 @@ const EditDatesModal = ({
                     {dates?.map((x: any, index: number) => {
                       return (
                         <div key={x.phase_id} className="text-tollgate-title">
-                          <span className='name-tollgate' style={{ marginBottom: '25px', color: invalidDateIndex === index ? 'red' : undefined }}>
+                          <span className='name-tollgate' style={{color: invalidDateIndex === index ? 'red' : undefined }}>
                             <span className="span-dots-tollgate">
                               <div className="toolgate-circle" style={{ backgroundColor: colorScale[paintCircle(index)] }} />
                             </span>
                             {x.name.replace(/([A-Z])/g, ' $1')}
                           </span>
-                          <span>
+                          <span className='dots-tollgate'>
                             {x.locked && <LockOutlined />}
                             <Dropdown overlay={menu(x, index)} placement="bottomRight">
                               <MoreOutlined />
